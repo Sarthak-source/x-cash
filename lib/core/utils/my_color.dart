3f61e7d2e750fd4d1848c5a6dfe9700d870c3d96
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-26 18:53:15 +0600

    added_app_colors

--- lib/core/utils/my_color.dart
+++ lib/core/utils/my_color.dart
@@ -1,3 +1,21 @@
+import 'package:flutter/material.dart';
+
 class MyColor{
 
+  static const Color primaryColor = Color(0xff7966FF);
+  static const Color primaryColor100 = Color(0xffF1EFFF);
+  static const Color primaryColor300 = Color(0xffC9C2FF);
+
+  static const Color primaryTextColor = Color(0xff292929);
+  static const Color primarySubTextColor = Color(0xff777777);
+  static const Color primaryStatusBarColor = Color(0xff634EF4);
+
+  static const Color underlineTextColor = Color(0xff7966FF);
+  static const Color lineColor = Color(0xffDBDFE8);
+  static const Color borderColor = Color(0xffD9D9D9);
+
+  static const Color colorWhite = Color(0xffFFFFFF);
+  static const Color colorGreen = Color(0xff28C76F);
+  static const Color colorOrange = Color(0xffFF9F43);
+  static const Color colorRed = Color(0xffEA5455);
 }
