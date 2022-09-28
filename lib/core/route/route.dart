716c295bf0e954eaeff42075dee2dad0e5dcf208
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-28 18:30:58 +0600

    added_login_screen_route

--- lib/core/route/route.dart
+++ lib/core/route/route.dart
@@ -1,15 +1,18 @@
 import 'package:get/get.dart';
+import 'package:xcash_app/view/screens/auth/login/login_screen.dart';
 import 'package:xcash_app/view/screens/onboard/onboard_screen.dart';
 import 'package:xcash_app/view/screens/splash/splash_screen.dart';
 
 class RouteHelper{
 
   static const String splashScreen = "/splash-screen";
-  static const String onboard = "/onboard-screen";
+  static const String onboardScreen = "/onboard-screen";
+  static const String loginScreen = "/login-screen";
 
   List<GetPage> routes = [
 
     GetPage(name: splashScreen, page: () => const SplashScreen()),
-    GetPage(name: onboard, page: () => const OnboardScreen())
+    GetPage(name: onboardScreen, page: () => const OnboardScreen()),
+    GetPage(name: loginScreen, page: () => const LoginScreen()),
   ];
 }
