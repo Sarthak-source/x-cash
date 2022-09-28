9e0586da9c107bf0ec6e42931cc687b9de789a39
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-28 11:26:43 +0600

    modify

--- lib/core/utils/dimensions.dart
+++ lib/core/utils/dimensions.dart
@@ -1,3 +1,24 @@
 class Dimensions{
 
+  static const double appLogoH = 85;
+  static const double appLogoV = 85;
+
+  // font-size
+  static const double fontExtraSmall = 10.00;
+  static const double fontSmall = 12.00;
+  static const double fontDefault = 14.00;
+  static const double fontDefaultLarge = 16.00;
+  static const double fontMediumLarge = 18.00;
+  static const double fontLarge = 20.00;
+  static const double fontExtraLarge = 22.00;
+  static const double fontOverLarge = 24.00;
+  static const double fontHeader = 26.00;
+  static const double fontHeader1 = 28.00;
+  static const double fontHeader2 = 30.00;
+  static const double fontHeader3 = 32.00;
+  static const double fontHeaderLarge = 45.00;
+
+  // button height
+  static const double defaultButtonH = 45;
+
 }
