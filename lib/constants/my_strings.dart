134f0e1c8d5d03726373f86108ed70d86d701b60
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-28 17:24:01 +0600

    modify

--- lib/constants/my_strings.dart
+++ lib/constants/my_strings.dart
@@ -1,12 +1,12 @@
 class MyStrings{
 
   // onboard-screen
-  static const String onboardTitle1 = "Welcome To Xcash Wallet App";
-  static const String onboardSubTitle1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry";
-  static const String onboardTitle2 = "Easy Way To Manage Your Money";
-  static const String onboardSubTitle2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry";
-  static const String onboardTitle3 = "Easy to view Your transaction report";
-  static const String onboardSubTitle3 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry";
+  static const String onboardTitle1 = "Welcome To Xcash\nWallet App";
+  static const String onboardSubTitle1 = "Lorem Ipsum is simply dummy text of the printing\nand type setting industry";
+  static const String onboardTitle2 = "Easy Way To Manage\nYour Money";
+  static const String onboardSubTitle2 = "Lorem Ipsum is simply dummy text of the printing\nand type setting industry";
+  static const String onboardTitle3 = "Easy to view your transaction\nreport";
+  static const String onboardSubTitle3 = "Lorem Ipsum is simply dummy text of the printing\nand type setting industry";
   static const String skip = "Skip";
   static const String next = "Next";
   static const String getStarted = "Get Started";
