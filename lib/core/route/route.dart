9a6353d1a09b1c66625afb7002afdcaccfcf79cb
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-28 11:39:49 +0600

    added_splash_route

--- lib/core/route/route.dart
+++ lib/core/route/route.dart
@@ -1,3 +1,12 @@
+import 'package:get/get.dart';
+import 'package:xcash_app/view/screens/splash/splash_screen.dart';
+
 class RouteHelper{
 
+  static const String splashScreen = "/splash-screen";
+
+  List<GetPage> routes = [
+
+    GetPage(name: splashScreen, page: () => const SplashScreen())
+  ];
 }
