d5dbf052d09f5b619fd09097cbcd02c68559962e
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-28 13:48:44 +0600

    added_onboard_screen_route

--- lib/core/route/route.dart
+++ lib/core/route/route.dart
@@ -1,12 +1,15 @@
 import 'package:get/get.dart';
+import 'package:xcash_app/view/screens/onboard/onboard_screen.dart';
 import 'package:xcash_app/view/screens/splash/splash_screen.dart';
 
 class RouteHelper{
 
   static const String splashScreen = "/splash-screen";
+  static const String onboard = "/onboard-screen";
 
   List<GetPage> routes = [
 
-    GetPage(name: splashScreen, page: () => const SplashScreen())
+    GetPage(name: splashScreen, page: () => const SplashScreen()),
+    GetPage(name: onboard, page: () => const OnboardScreen())
   ];
 }
