58188c9745a069a17baa1c304ece801d229dcaa7
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-28 18:31:39 +0600

    create_login_screen

---
+++ lib/view/screens/auth/login/login_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/styles.dart';
+import 'package:xcash_app/core/utils/util.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  @override
+  void initState() {
+    MyUtil.secondaryTheme();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    MyUtil.secondaryTheme();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: MyColor.colorWhite,
+
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(MyStrings.welcomeBack, textAlign: TextAlign.left, style: interRegularLarge.copyWith(fontWeight: FontWeight.w600)),
+                const SizedBox(height: 10),
+                Text(MyStrings.loginSubTitle, textAlign: TextAlign.left, style: interRegularSmall.copyWith(color: MyColor.primarySubTextColor)),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
