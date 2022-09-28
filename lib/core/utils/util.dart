68f55624ad71c52d107f0c5c2ccc7d3794cc7c39
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-28 12:03:58 +0600

    add_primary_util_for theme

--- lib/core/utils/util.dart
+++ lib/core/utils/util.dart
@@ -1,3 +1,13 @@
+import 'package:flutter/services.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+
 class MyUtil{
 
+  static primaryTheme(){
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: MyColor.primaryColor,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: MyColor.primaryColor,
+        systemNavigationBarIconBrightness: Brightness.light));
+  }
 }
