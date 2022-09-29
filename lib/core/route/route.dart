2c4ad0ab32c8c54a15a13cecdd4fb7199ab80739
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-29 13:48:44 +0600

    forgot_password_screen_route_added

--- lib/core/route/route.dart
+++ lib/core/route/route.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:xcash_app/view/screens/auth/forgot_password/forgot_password_screen.dart';
 import 'package:xcash_app/view/screens/auth/login/login_screen.dart';
 import 'package:xcash_app/view/screens/onboard/onboard_screen.dart';
 import 'package:xcash_app/view/screens/splash/splash_screen.dart';
@@ -8,11 +9,13 @@ class RouteHelper{
   static const String splashScreen = "/splash-screen";
   static const String onboardScreen = "/onboard-screen";
   static const String loginScreen = "/login-screen";
+  static const String forgotPasswordScreen = "/forgot-password-screen";
 
   List<GetPage> routes = [
 
     GetPage(name: splashScreen, page: () => const SplashScreen()),
     GetPage(name: onboardScreen, page: () => const OnboardScreen()),
     GetPage(name: loginScreen, page: () => const LoginScreen()),
+    GetPage(name: forgotPasswordScreen, page: () => const ForgotPasswordScreen()),
   ];
 }
