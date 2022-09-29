73d6a8b4610f9eb35db94e9f27b21b1d89f7f33f
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-29 13:48:28 +0600

    created_forgot_password_screen

---
+++ lib/view/screens/auth/forgot_password/forgot_password_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/view/components/text/app_bar_text.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  const ForgotPasswordScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: MyColor.primaryColor100,
+        appBar: AppBar(
+          backgroundColor: MyColor.colorWhite,
+          elevation: 0,
+          title: const AppBarText(text: MyStrings.forgotPassword),
+          leading: GestureDetector(
+            onTap: () => Get.back(),
+            child: const Icon(Icons.arrow_back, color: MyColor.primaryColor, size: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
