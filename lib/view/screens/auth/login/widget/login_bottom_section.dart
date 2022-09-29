73a7a49dd999c8f709d56aed5f9d3f9af0b253b0
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-29 12:20:55 +0600

    created_new_widget_for_login_screen

---
+++ lib/view/screens/auth/login/widget/login_bottom_section.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/styles.dart';
+
+class LoginBottomSection extends StatefulWidget {
+  const LoginBottomSection({Key? key}) : super(key: key);
+
+  @override
+  State<LoginBottomSection> createState() => _LoginBottomSectionState();
+}
+
+class _LoginBottomSectionState extends State<LoginBottomSection> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+
+        Text(MyStrings.donotAccount, style: interRegularDefault),
+        TextButton(
+
+          onPressed: () {
+            //Get.offAllNamed(RouteHelper.registrationScreen);
+          },
+          child: Text(
+
+            MyStrings.registerNow,
+            style: interRegularDefault.copyWith(color: MyColor.primaryColor, decoration: TextDecoration.underline),
+          ),
+        )
+      ],
+    );
+  }
+}
