f73991c4af4dd9a077c90440e63074098a1e4773
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-29 18:25:45 +0600

    registration_route_added

--- lib/core/route/route.dart
+++ lib/core/route/route.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:xcash_app/view/screens/auth/change_password/change_password_screen.dart';
 import 'package:xcash_app/view/screens/auth/forgot_password/forgot_password_screen.dart';
 import 'package:xcash_app/view/screens/auth/login/login_screen.dart';
+import 'package:xcash_app/view/screens/auth/registration/registration_screen.dart';
 import 'package:xcash_app/view/screens/onboard/onboard_screen.dart';
 import 'package:xcash_app/view/screens/splash/splash_screen.dart';
 
@@ -12,6 +13,7 @@ class RouteHelper{
   static const String loginScreen = "/login-screen";
   static const String forgotPasswordScreen = "/forgot-password-screen";
   static const String changePasswordScreen = "/change-password-screen";
+  static const String registrationScreen = "/registration-screen";
 
   List<GetPage> routes = [
 
@@ -20,5 +22,6 @@ class RouteHelper{
     GetPage(name: loginScreen, page: () => const LoginScreen()),
     GetPage(name: forgotPasswordScreen, page: () => const ForgotPasswordScreen()),
     GetPage(name: changePasswordScreen, page: () => const ChangePasswordScreen()),
+    GetPage(name: registrationScreen, page: () => const RegistrationScreen()),
   ];
 }
