d498e5495f522bca56b0d14c3965801ccbfaedda
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-29 12:30:22 +0600

    modify

--- lib/view/components/text/small_text.dart
+++ lib/view/components/text/small_text.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
 import 'package:xcash_app/core/utils/styles.dart';
 
 class SmallText extends StatelessWidget {
   final String text;
   final Color textColor;
-  const SmallText({Key? key, required this.text, this.textColor = MyColor.primaryTextColor}) : super(key: key);
+  final double fontSize;
+  const SmallText({Key? key, required this.text, this.textColor = MyColor.primaryTextColor, this.fontSize = Dimensions.fontSmall}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: interRegularSmall.copyWith(color: textColor, fontWeight: FontWeight.w400),
+      style: interRegularSmall.copyWith(color: textColor, fontWeight: FontWeight.w400, fontSize: fontSize),
     );
   }
 }
