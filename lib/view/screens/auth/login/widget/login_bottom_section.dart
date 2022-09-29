9004a0fe78587c6d5031da8a1aacccddbbef40c3
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-29 18:27:30 +0600

    modify

--- lib/view/screens/auth/login/widget/login_bottom_section.dart
+++ lib/view/screens/auth/login/widget/login_bottom_section.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/route/route.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
 import 'package:xcash_app/core/utils/styles.dart';
 
@@ -22,7 +24,7 @@ class _LoginBottomSectionState extends State<LoginBottomSection> {
         TextButton(
 
           onPressed: () {
-            //Get.offAllNamed(RouteHelper.registrationScreen);
+            Get.offAllNamed(RouteHelper.registrationScreen);
           },
           child: Text(
 
