cff64b17b33b02ec09fc1e718868de9fa6b4921c
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-09-29 15:14:51 +0600

    add_color

--- lib/core/utils/my_color.dart
+++ lib/core/utils/my_color.dart
@@ -14,7 +14,7 @@ class MyColor{
   static const Color lineColor = Color(0xffDBDFE8);
   static const Color borderColor = Color(0xffD9D9D9);
 
-  static const Color textFieldBorderColor = Color(0xffEBEBEB);
+  static const Color textFieldBorderColor = Color(0xffCFCEDB);
 
   static const Color colorWhite = Color(0xffFFFFFF);
   static const Color colorGreen = Color(0xff28C76F);
