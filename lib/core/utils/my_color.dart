c90f2dbee64f71886ed119ad8b5b11d421b065f5
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-15 14:23:49 +0600

    change_color

--- lib/core/utils/my_color.dart
+++ lib/core/utils/my_color.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class MyColor{
 
   static const Color primaryColor = Color(0xff7966FF);
+  static const Color secondaryColor = Color(0xffF6F7FE);
+  static const Color bodyColor = Color(0xff555555);
+
   static const Color primaryColor100 = Color(0xffF1EFFF);
   static const Color primaryColor300 = Color(0xffC9C2FF);
 
@@ -11,7 +14,7 @@ class MyColor{
   static const Color primaryStatusBarColor = Color(0xff634EF4);
 
   static const Color underlineTextColor = Color(0xff7966FF);
-  static const Color lineColor = Color(0xffDBDFE8);
+  static const Color lineColor = Color(0xffECECEC);
   static const Color borderColor = Color(0xffD9D9D9);
 
   static const Color textFieldBorderColor = Color(0xffCFCEDB);
@@ -20,6 +23,5 @@ class MyColor{
   static const Color colorGreen = Color(0xff28C76F);
   static const Color colorOrange = Color(0xffFF9F43);
   static const Color colorRed = Color(0xffEA5455);
-
   static const Color transparentColor = Colors.transparent;
 }
