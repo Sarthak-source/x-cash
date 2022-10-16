05b1d79a7540934b69efbf1e27130c63d19f93cb
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-16 15:41:40 +0600

    create_home_screen

--- lib/view/screens/home/home_screen.dart
+++ lib/view/screens/home/home_screen.dart
@@ -1,16 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/view/screens/home/widget/top_section.dart';
+import 'package:xcash_app/view/screens/home/widget/wallet_section.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: MyColor.primaryColor,
-        body: Center(
-          child: Text("Home Screen"),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: false,
+              elevation: 0,
+              automaticallyImplyLeading: false,
+              backgroundColor: MyColor.primaryColor,
+              expandedHeight: 200,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Padding(
+                  padding: const EdgeInsets.only(left: Dimensions.space15, right: Dimensions.space15, top: Dimensions.space30),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      TopSection(),
+
+                      SizedBox(height: Dimensions.space30),
+
+                      WalletSection(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+
+            SliverToBoxAdapter(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                margin: EdgeInsets.only(top: 100),
+                color: MyColor.primaryColor100,
+              ),
+            )
+          ],
         ),
       ),
     );
