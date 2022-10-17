744fc6f2df668de4f8807b6feb792cebbc1a0f98
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-17 13:11:53 +0600

    add_navigation_route

--- lib/view/screens/home/widget/wallet_section.dart
+++ lib/view/screens/home/widget/wallet_section.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/route/route.dart';
 import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
 import 'package:xcash_app/core/utils/styles.dart';
@@ -18,9 +20,20 @@ class WalletSection extends StatelessWidget {
             MyStrings.myWallet,
             style: interRegularDefault.copyWith(color: MyColor.colorWhite, fontWeight: FontWeight.w500),
           ),
-          Text(
-            MyStrings.viewMore,
-            style: interRegularSmall.copyWith(color: MyColor.colorWhite),
+          GestureDetector(
+            onTap: (){
+              Get.toNamed(RouteHelper.myWalletScreen);
+            },
+            child: Container(
+              alignment: Alignment.center,
+              color: MyColor.transparentColor,
+              padding: const EdgeInsets.all(Dimensions.space5),
+              child: Text(
+                MyStrings.viewMore,
+                textAlign: TextAlign.center,
+                style: interRegularSmall.copyWith(color: MyColor.colorWhite),
+              ),
+            ),
           )
         ],
       ),
