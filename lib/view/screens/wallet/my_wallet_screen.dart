ac195500a96f83c048733f40d49cdc3c8ee20818
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-17 12:48:24 +0600

    create_my_wallet_screen

---
+++ lib/view/screens/wallet/my_wallet_screen.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/my_images.dart';
+import 'package:xcash_app/view/components/card/custom_card.dart';
+
+class MyWalletScreen extends StatefulWidget {
+  const MyWalletScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MyWalletScreen> createState() => _MyWalletScreenState();
+}
+
+class _MyWalletScreenState extends State<MyWalletScreen> {
+
+  List<Map<String, String>> data = [
+    {
+      "image" : MyImages.transferMoney,
+      "amount" : "3,481,070,153.92",
+      "currency" : "USD"
+    },
+    {
+      "image" : MyImages.transferMoney,
+      "amount" : "3,481,070,153.92",
+      "currency" : "USD"
+    },
+    {
+      "image" : MyImages.transferMoney,
+      "amount" : "3,481,070,153.92",
+      "currency" : "USD"
+    },
+    {
+      "image" : MyImages.transferMoney,
+      "amount" : "3,481,070,153.92",
+      "currency" : "USD"
+    },
+    {
+      "image" : MyImages.transferMoney,
+      "amount" : "3,481,070,153.92",
+      "currency" : "USD"
+    },
+    {
+      "image" : MyImages.transferMoney,
+      "amount" : "3,481,070,153.92",
+      "currency" : "NGN"
+    },
+    {
+      "image" : MyImages.transferMoney,
+      "amount" : "0.92134943",
+      "currency" : "ETH"
+    },
+    {
+      "image" : MyImages.transferMoney,
+      "amount" : "8,578.96",
+      "currency" : "BDT"
+    },
+    {
+      "image" : MyImages.transferMoney,
+      "amount" : "0.00",
+      "currency" : "INR"
+    },
+    {
+      "image" : MyImages.transferMoney,
+      "amount" : "0.000000000",
+      "currency" : "BCH"
+    }
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: MyColor.primaryColor100,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Dimensions.space15, vertical: Dimensions.space20),
+          child: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            child: GridView.builder(
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              itemCount: data.length,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 12,
+                mainAxisSpacing: 12,
+              ),
+              itemBuilder: (context, index) => CustomCard(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
