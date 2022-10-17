83cd736ad867ddc76e079280dfe3175243c99300
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-17 15:28:56 +0600

    create_horizontal_icon_text_widget

---
+++ lib/view/components/row_icon_text_widget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/styles.dart';
+import 'package:xcash_app/view/components/image/circle_shape_image.dart';
+
+class RowIconTextWidget extends StatelessWidget {
+  final VoidCallback? onPressed;
+  bool isSvgPicture;
+  final String image;
+  final String text;
+  RowIconTextWidget({
+    Key? key,
+    this.onPressed,
+    this.isSvgPicture = false,
+    required this.image,
+    required this.text
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return isSvgPicture ? GestureDetector(
+      onTap: onPressed,
+      child: Row(
+        children: [
+          CircleShapeImage(
+            isSvgImage: isSvgPicture,
+            image: image,
+            imageColor: MyColor.primaryTextColor,
+            imageSize: 16,
+          ),
+
+          const SizedBox(width: Dimensions.space20),
+
+          Text(text, style: interRegularSmall.copyWith(fontWeight: FontWeight.w500))
+        ],
+      ),
+    ) : GestureDetector(
+      onTap: onPressed,
+      child: Row(
+        children: [
+          CircleShapeImage(
+            isSvgImage: isSvgPicture,
+            image: image,
+            imageColor: MyColor.primaryTextColor,
+            imageSize: 16,
+          ),
+
+          const SizedBox(width: Dimensions.space20),
+
+          Text(text, style: interRegularSmall.copyWith(fontWeight: FontWeight.w500))
+        ],
+      ),
+    );
+  }
+}
