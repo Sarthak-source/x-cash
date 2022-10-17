b3aaed136edad240c7bba6652cdb7447c4d4b5e8
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-17 13:10:40 +0600

    create_image_widget

---
+++ lib/view/components/image/circle_shape_image.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+
+class CircleShapeImage extends StatefulWidget {
+  final String image;
+  final Color backgroundColor, imageColor;
+  final double imageSize;
+  bool isSvgImage;
+  CircleShapeImage({
+    Key? key,
+    this.backgroundColor = MyColor.primaryColor100,
+    this.imageColor = MyColor.primaryColor,
+    this.imageSize = 16,
+    required this.image,
+    this.isSvgImage = false
+  }) : super(key: key);
+
+  @override
+  State<CircleShapeImage> createState() => _CircleShapeImageState();
+}
+
+class _CircleShapeImageState extends State<CircleShapeImage> {
+  @override
+  Widget build(BuildContext context) {
+    return widget.isSvgImage ? Container(
+      padding: const EdgeInsets.all(Dimensions.space10),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: widget.backgroundColor,
+        shape: BoxShape.circle
+      ),
+      child: SvgPicture.asset(widget.image, color: widget.imageColor, height: widget.imageSize, width: widget.imageSize),
+    ) : Container(
+      padding: const EdgeInsets.all(Dimensions.space10),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          color: widget.backgroundColor,
+          shape: BoxShape.circle
+      ),
+      child: Image.asset(widget.image, color: widget.imageColor, height: widget.imageSize, width: widget.imageSize),
+    );
+  }
+}
