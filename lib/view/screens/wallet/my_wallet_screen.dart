f179e26371cfba635836dd05969db7289866a0f5
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-17 14:44:39 +0600

    my_wallet_screen_design_complete

--- lib/view/screens/wallet/my_wallet_screen.dart
+++ lib/view/screens/wallet/my_wallet_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/route/route.dart';
 import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
 import 'package:xcash_app/core/utils/my_images.dart';
+import 'package:xcash_app/core/utils/styles.dart';
 import 'package:xcash_app/view/components/app-bar/custom_app_bar.dart';
 import 'package:xcash_app/view/components/card/custom_card.dart';
+import 'package:xcash_app/view/components/divider/custom_divider.dart';
+import 'package:xcash_app/view/components/image/circle_shape_image.dart';
 
 class MyWalletScreen extends StatefulWidget {
   const MyWalletScreen({Key? key}) : super(key: key);
@@ -67,8 +71,12 @@ class _MyWalletScreenState extends State<MyWalletScreen> {
       "currency" : "BCH"
     }
   ];
+
   @override
   Widget build(BuildContext context) {
+
+    double width = MediaQuery.of(context).size.width;
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: MyColor.primaryColor100,
@@ -76,18 +84,20 @@ class _MyWalletScreenState extends State<MyWalletScreen> {
           elevation: 0,
           showBackIcon: true,
           title: MyStrings.myWallet,
+          routeName: RouteHelper.bottomNavBar,
         ),
         body: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: Dimensions.space15, vertical: Dimensions.space20),
-          physics: const BouncingScrollPhysics(),
+          physics: const ClampingScrollPhysics(),
           child: GridView.builder(
             shrinkWrap: true,
             padding: EdgeInsets.zero,
             scrollDirection: Axis.vertical,
             itemCount: data.length,
             physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
+              childAspectRatio: width > 400 ? 2.4 : 1,
               crossAxisSpacing: 12,
               mainAxisSpacing: 12,
             ),
@@ -96,9 +106,33 @@ class _MyWalletScreenState extends State<MyWalletScreen> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    height: 30, width: 30,
+                  CircleShapeImage(
+                    isSvgImage: true,
+                    image: data[index]["image"].toString(),
+                  ),
 
+                  const SizedBox(height: Dimensions.space10),
+
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(text: data[index]["amount"], style: interRegularDefault.copyWith(fontWeight: FontWeight.w600)),
+                        TextSpan(text: " ${data[index]["currency"]}", style: interRegularExtraSmall.copyWith(fontWeight: FontWeight.w600))
+                      ]
+                    ),
+                  ),
+
+                  const CustomDivider(),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        MyStrings.transferMoney,
+                        style: interRegularSmall.copyWith(color: MyColor.primarySubTextColor),
+                      ),
+                      const Icon(Icons.arrow_forward_ios_outlined, color: MyColor.primaryColor, size: 16)
+                    ],
                   )
                 ],
               ),
