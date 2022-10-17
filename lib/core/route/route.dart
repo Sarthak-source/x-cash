34b2615601110e4be7fdd370fa071998e18865ce
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-17 12:47:10 +0600

    add_new_route

--- lib/core/route/route.dart
+++ lib/core/route/route.dart
@@ -6,6 +6,7 @@ import 'package:xcash_app/view/screens/auth/login/login_screen.dart';
 import 'package:xcash_app/view/screens/auth/registration/registration_screen.dart';
 import 'package:xcash_app/view/screens/onboard/onboard_screen.dart';
 import 'package:xcash_app/view/screens/splash/splash_screen.dart';
+import 'package:xcash_app/view/screens/wallet/my_wallet_screen.dart';
 
 class RouteHelper{
 
@@ -15,6 +16,7 @@ class RouteHelper{
   static const String forgotPasswordScreen = "/forgot_password_screen";
   static const String changePasswordScreen = "/change_password_screen";
   static const String registrationScreen = "/registration_screen";
+  static const String myWalletScreen = "/my_wallet_screen";
 
   static const String bottomNavBar = "/bottom_nav_bar";
 
@@ -26,6 +28,7 @@ class RouteHelper{
     GetPage(name: forgotPasswordScreen, page: () => const ForgotPasswordScreen()),
     GetPage(name: changePasswordScreen, page: () => const ChangePasswordScreen()),
     GetPage(name: registrationScreen, page: () => const RegistrationScreen()),
-    GetPage(name: bottomNavBar, page: () => const BottomNavBar())
+    GetPage(name: bottomNavBar, page: () => const BottomNavBar()),
+    GetPage(name: myWalletScreen, page: () => const MyWalletScreen())
   ];
 }
