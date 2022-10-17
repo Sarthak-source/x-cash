0cdb8144885858457e2cc2f5ae0426526ac66d2f
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-17 17:03:19 +0600

    activity_screen_design_complete

--- lib/view/screens/activity/activity_screen.dart
+++ lib/view/screens/activity/activity_screen.dart
@@ -1,18 +1,72 @@
 import 'package:flutter/material.dart';
-import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/core/utils/my_images.dart';
+import 'package:xcash_app/view/components/bottom-sheet/bottom_sheet_bar.dart';
+import 'package:xcash_app/view/components/divider/custom_divider.dart';
+import 'package:xcash_app/view/components/row_icon_text_widget.dart';
 
 class ActivityScreen extends StatelessWidget {
+
   const ActivityScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: MyColor.primaryColor,
-        body: Center(
-          child: Text("Activity Screen"),
-        ),
-      ),
+    return Column(
+      children: [
+        const BottomSheetBar(),
+        const SizedBox(height: Dimensions.space15),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RowIconTextWidget(
+                isSvgPicture: true,
+                image: MyImages.transaction,
+                text: MyStrings.transaction
+            ),
+
+            const CustomDivider(height: Dimensions.space15),
+
+            RowIconTextWidget(
+                isSvgPicture: true,
+                image: MyImages.requestMe,
+                text: MyStrings.requestToMe
+            ),
+
+            const CustomDivider(height: Dimensions.space15),
+
+            RowIconTextWidget(
+                isSvgPicture: true,
+                image: MyImages.addMoneyHistory,
+                text: MyStrings.addMoneyHistory
+            ),
+
+            const CustomDivider(height: Dimensions.space15),
+
+            RowIconTextWidget(
+                isSvgPicture: true,
+                image: MyImages.myVoucher,
+                text: MyStrings.myVoucher
+            ),
+
+            const CustomDivider(height: Dimensions.space15),
+
+            RowIconTextWidget(
+                isSvgPicture: true,
+                image: MyImages.withdrawHistory,
+                text: MyStrings.withdrawHistory
+            ),
+
+            const CustomDivider(height: Dimensions.space15),
+
+            RowIconTextWidget(
+                isSvgPicture: true,
+                image: MyImages.myInvoice,
+                text: MyStrings.myInvoice
+            ),
+          ],
+        )
+      ],
     );
   }
 }
