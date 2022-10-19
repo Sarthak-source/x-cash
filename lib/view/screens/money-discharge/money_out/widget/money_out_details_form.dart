247326bc2b9256af8a158244cd87834be3ef2929
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-19 14:28:15 +0600

    create_money_out_details_form

---
+++ lib/view/screens/money-discharge/money_out/widget/money_out_details_form.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MoneyOutDetailsForm extends StatefulWidget {
+  const MoneyOutDetailsForm({Key? key}) : super(key: key);
+
+  @override
+  State<MoneyOutDetailsForm> createState() => _MoneyOutDetailsFormState();
+}
+
+class _MoneyOutDetailsFormState extends State<MoneyOutDetailsForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
