cad1057c2b08bdc6da8b872d4a4f316506702d78
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-19 14:23:40 +0600

    create_money_out_details

---
+++ lib/view/screens/money-discharge/money_out/money_out_details.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/view/components/bottom-sheet/bottom_sheet_close_button.dart';
+import 'package:xcash_app/view/components/divider/custom_divider.dart';
+import 'package:xcash_app/view/components/text/bottom_sheet_header_text.dart';
+
+class MoneyOutDetails extends StatelessWidget {
+  const MoneyOutDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            BottomSheetHeaderText(text: MyStrings.moneyOutDetail),
+            BottomSheetCloseButton()
+          ],
+        ),
+
+        const CustomDivider(height: Dimensions.space15),
+      ],
+    );
+  }
+}
+
