782448d332cf290d434c39a0e5acc32d875b4834
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-19 14:39:45 +0600

    add_form

--- lib/view/screens/money-discharge/money_out/money_out_details.dart
+++ lib/view/screens/money-discharge/money_out/money_out_details.dart
@@ -4,6 +4,7 @@ import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/view/components/bottom-sheet/bottom_sheet_close_button.dart';
 import 'package:xcash_app/view/components/divider/custom_divider.dart';
 import 'package:xcash_app/view/components/text/bottom_sheet_header_text.dart';
+import 'package:xcash_app/view/screens/money-discharge/money_out/widget/money_out_details_form.dart';
 
 class MoneyOutDetails extends StatelessWidget {
   const MoneyOutDetails({Key? key}) : super(key: key);
@@ -22,6 +23,8 @@ class MoneyOutDetails extends StatelessWidget {
         ),
 
         const CustomDivider(height: Dimensions.space15),
+
+        const MoneyOutDetailsForm()
       ],
     );
   }
