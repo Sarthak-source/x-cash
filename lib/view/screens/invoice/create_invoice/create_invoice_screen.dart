f9628fd303cba58c4b48f38e69fcab309e3128f7
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-19 15:40:35 +0600

    create_create_invoice_screen

---
+++ lib/view/screens/invoice/create_invoice/create_invoice_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/route/route.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/view/components/app-bar/custom_app_bar.dart';
+
+class CreateInvoiceScreen extends StatelessWidget {
+  const CreateInvoiceScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: MyColor.primaryColor100,
+        appBar: CustomAppBar(
+          showBackIcon: true,
+          title: MyStrings.createInvoice,
+          changeRoute: () => Get.toNamed(RouteHelper.myInvoiceScreen),
+        ),
+      ),
+    );
+  }
+}
