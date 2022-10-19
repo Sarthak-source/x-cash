9ae78503c8578645fa58ae7862224287af8d01f8
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-19 13:23:45 +0600

    create_new_bottom_sheet_widget

---
+++ lib/view/components/bottom-sheet/bottom_sheet_close_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+
+class BottomSheetCloseButton extends StatelessWidget {
+  const BottomSheetCloseButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Get.back(),
+      child: Container(
+        height: 30, width: 30,
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(Dimensions.space5),
+        decoration: const BoxDecoration(color: MyColor.primaryColor100, shape: BoxShape.circle),
+        child: const Icon(Icons.clear, color: MyColor.colorBlack, size: 15),
+      ),
+    );
+  }
+}
