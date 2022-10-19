74fc0a38a33a4d5f7c6bb75bd72d4d153cab1659
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-19 17:20:12 +0600

    design_complete_and_add_both_invoice_details_and_items

--- lib/view/screens/invoice/create_invoice/create_invoice_screen.dart
+++ lib/view/screens/invoice/create_invoice/create_invoice_screen.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:xcash_app/constants/my_strings.dart';
 import 'package:xcash_app/core/route/route.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
 import 'package:xcash_app/view/components/app-bar/custom_app_bar.dart';
+import 'package:xcash_app/view/screens/invoice/create_invoice/widget/invoice_details.dart';
+import 'package:xcash_app/view/screens/invoice/create_invoice/widget/invoice_items.dart';
 
 class CreateInvoiceScreen extends StatelessWidget {
   const CreateInvoiceScreen({Key? key}) : super(key: key);
@@ -15,8 +18,21 @@ class CreateInvoiceScreen extends StatelessWidget {
         backgroundColor: MyColor.primaryColor100,
         appBar: CustomAppBar(
           showBackIcon: true,
+          elevation: 0,
           title: MyStrings.createInvoice,
           changeRoute: () => Get.toNamed(RouteHelper.myInvoiceScreen),
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(vertical: Dimensions.space20, horizontal: Dimensions.space15),
+          child: Column(
+            children: const [
+              InvoiceDetails(),
+
+              SizedBox(height: Dimensions.space20),
+
+              InvoiceItems()
+            ],
+          ),
         ),
       ),
     );
