f3433d76e20db4a01b7783907dae2647c8b7c598
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-19 15:05:32 +0600

    complete_payment_details_form

---
+++ lib/view/screens/money-discharge/make_payment/widget/payment_details_form.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/styles.dart';
+import 'package:xcash_app/view/components/buttons/custom_animated_button.dart';
+import 'package:xcash_app/view/components/text-form-field/custom_amount_text_field.dart';
+import 'package:xcash_app/view/components/text-form-field/custom_drop_down_text_field.dart';
+import 'package:xcash_app/view/components/text-form-field/custom_text_form_field.dart';
+
+class PaymentDetailsForm extends StatefulWidget {
+  const PaymentDetailsForm({Key? key}) : super(key: key);
+
+  @override
+  State<PaymentDetailsForm> createState() => _PaymentDetailsFormState();
+}
+
+class _PaymentDetailsFormState extends State<PaymentDetailsForm> {
+
+  var selectWalletValue = "Select Wallet";
+  var selectOtp = "Select OTP Type";
+
+  List<String> walletItems = ["Select Wallet", "TRX", "USD", "BCH", "BDT", "BTC", "BTL", "CNY", "ETH", "EUR"];
+  List<String> otpItems = ["Select OTP Type", "Email", "Sms"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomTextFormField(
+              labelText: MyStrings.merchantUsernameEmail,
+              hintText: MyStrings.merchantUsernameEmailHint,
+              onChanged: (value){}
+          ),
+
+          const SizedBox(height: Dimensions.space15),
+
+          CustomDropDownTextField(
+              labelText: MyStrings.selectWallet,
+              hintText: selectWalletValue,
+              selectedValue: selectWalletValue,
+              onChanged: (value){
+                setState(() {
+                  selectWalletValue = value.toString();
+                });
+              },
+              items: walletItems.map((String val){
+                return DropdownMenuItem(
+                    value: val,
+                    child: Text(
+                      val,
+                      style: interRegularSmall,
+                    )
+                );
+              }).toList()
+          ),
+
+          const SizedBox(height: Dimensions.space15),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomAmountTextField(
+                  labelText: MyStrings.amount,
+                  hintText: MyStrings.amountHint,
+                  onChanged: (value){}
+              ),
+              const SizedBox(height: Dimensions.space5),
+              Text(MyStrings.min_max_Amount, style: interRegularExtraSmall.copyWith(color: MyColor.primaryColor))
+            ],
+          ),
+          const SizedBox(height: Dimensions.space15),
+
+          CustomDropDownTextField(
+              labelText: MyStrings.selectOtp,
+              hintText: selectOtp,
+              selectedValue: selectOtp,
+              onChanged: (value){
+                setState(() {
+                  selectOtp = value.toString();
+                });
+              },
+              items: otpItems.map((String val){
+                return DropdownMenuItem(
+                    value: val,
+                    child: Text(
+                      val,
+                      style: interRegularSmall,
+                    )
+                );
+              }).toList()
+          ),
+
+          const SizedBox(height: Dimensions.space20),
+
+          CustomAnimatedButton(
+            onTap: (){},
+            height: 45,
+            width: MediaQuery.of(context).size.width,
+            backgroundColor: MyColor.primaryColor,
+            child: Text(MyStrings.makePayment, textAlign: TextAlign.center, style: interRegularDefault.copyWith(color: MyColor.colorWhite, fontWeight: FontWeight.w600)),
+          )
+        ],
+      ),
+    );
+  }
+}
