7ac8868ee4d05cdaa6b61c8cd0ee2f5d30c8bfd2
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-20 11:53:12 +0600

    card_design_complete

--- lib/view/screens/voucher/my_voucher/widget/voucher_used.dart
+++ lib/view/screens/voucher/my_voucher/widget/voucher_used.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/styles.dart';
+import 'package:xcash_app/view/components/card/custom_card.dart';
+import 'package:xcash_app/view/components/divider/custom_divider.dart';
 
 class VoucherUsed extends StatefulWidget {
   const VoucherUsed({Key? key}) : super(key: key);
@@ -10,6 +13,30 @@ class VoucherUsed extends StatefulWidget {
 }
 
 class _VoucherUsedState extends State<VoucherUsed> {
+  
+  List<Map<String, dynamic>> data = [
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+    {"code" : "6775-2717-6927-2945", "useStatus" :  "USED", "createDate" : "12 Jan, 2022", "amount" : 489.00, "usedAtDate" : "12 Sep, 2022", "usedAtTime" : "6.00"},
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -19,12 +46,63 @@ class _VoucherUsedState extends State<VoucherUsed> {
           padding: EdgeInsets.zero,
           scrollDirection: Axis.vertical,
           physics: const BouncingScrollPhysics(),
-          itemCount: 20,
+          itemCount: data.length,
           separatorBuilder: (context, index) => const SizedBox(height: Dimensions.space10),
-          itemBuilder: (context, index) => Container(
-            height: 45,
+          itemBuilder: (context, index) => CustomCard(
+            paddingTop: Dimensions.space15,
+            paddingBottom: Dimensions.space15,
             width: MediaQuery.of(context).size.height,
-            color: MyColor.colorRed,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(data[index]["code"], style: interRegularDefault.copyWith(fontWeight: FontWeight.w500)),
+                    Container(
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.symmetric(vertical: Dimensions.space5 / 2, horizontal: Dimensions.space5),
+                      decoration: BoxDecoration(
+                          color: MyColor.colorOrange100,
+                          borderRadius: BorderRadius.circular(3),
+                          border: Border.all(color: MyColor.colorOrange, width: 0.5)
+                      ),
+                      child: Text(
+                          "${data[index]['useStatus']}",
+                          textAlign: TextAlign.center,
+                          style: interRegularExtraSmall.copyWith(
+                              color: MyColor.colorOrange,
+                              fontWeight: FontWeight.w500
+                          )
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: Dimensions.space5),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text("Create at : ${data[index]["createDate"]}", style: interRegularSmall.copyWith(color: MyColor.primarySubTextColor)),
+                    Text.rich(
+                        TextSpan(
+                            children: [
+                              TextSpan(text: data[index]["amount"].toString(), style: interRegularDefaultLarge.copyWith(fontWeight: FontWeight.w600)),
+                              TextSpan(text: " USD", style: interRegularSmall)
+                            ]
+                        )
+                    )
+                  ],
+                ),
+
+                const CustomDivider(height: Dimensions.space15),
+
+                Text("Used At - ${data[index]["usedAtDate"]} - ${data[index]["usedAtTime"]} am", style: interRegularSmall)
+              ],
+            ),
           )
       ),
     );
