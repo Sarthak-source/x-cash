40f8df820b871a9395f84c5201ad37de384ca731
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-20 14:56:11 +0600

    create_screen

---
+++ lib/view/screens/withdrawals/withdraw_money/withdraw_money_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WithdrawMoneyScreen extends StatefulWidget {
+  const WithdrawMoneyScreen({Key? key}) : super(key: key);
+
+  @override
+  State<WithdrawMoneyScreen> createState() => _WithdrawMoneyScreenState();
+}
+
+class _WithdrawMoneyScreenState extends State<WithdrawMoneyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
