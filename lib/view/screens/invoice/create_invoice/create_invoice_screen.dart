2f146486bbe65bcb4847222a8ceb6ee21eb58bf1
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-20 19:01:42 +0600

    add_button

--- lib/view/screens/invoice/create_invoice/create_invoice_screen.dart
+++ lib/view/screens/invoice/create_invoice/create_invoice_screen.dart
@@ -4,7 +4,9 @@ import 'package:xcash_app/constants/my_strings.dart';
 import 'package:xcash_app/core/route/route.dart';
 import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/styles.dart';
 import 'package:xcash_app/view/components/app-bar/custom_app_bar.dart';
+import 'package:xcash_app/view/components/buttons/custom_animated_button.dart';
 import 'package:xcash_app/view/screens/invoice/create_invoice/widget/invoice_details.dart';
 import 'package:xcash_app/view/screens/invoice/create_invoice/widget/invoice_items.dart';
 
@@ -25,12 +27,26 @@ class CreateInvoiceScreen extends StatelessWidget {
         body: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(vertical: Dimensions.space20, horizontal: Dimensions.space15),
           child: Column(
-            children: const [
-              InvoiceDetails(),
+            children: [
+              const InvoiceDetails(),
 
-              SizedBox(height: Dimensions.space20),
+              const SizedBox(height: Dimensions.space20),
 
-              InvoiceItems()
+              const InvoiceItems(),
+
+              const SizedBox(height: Dimensions.space25),
+
+              CustomAnimatedButton(
+                  onTap: (){},
+                  height: 45,
+                  width: MediaQuery.of(context).size.width,
+                  backgroundColor: MyColor.primaryColor,
+                  child: Text(
+                    MyStrings.createInvoice,
+                    textAlign: TextAlign.center,
+                    style: interRegularDefault.copyWith(color: MyColor.colorWhite, fontWeight: FontWeight.w500),
+                  ),
+              )
             ],
           ),
         ),
