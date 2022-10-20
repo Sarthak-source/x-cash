25753dc6390afe090cff070470a653a88e5b586c
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-20 17:25:54 +0600

    add_withdraw_method_form

---
+++ lib/view/screens/withdrawals/withdraw_method/withdraw_method.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/view/components/bottom-sheet/bottom_sheet_close_button.dart';
+import 'package:xcash_app/view/components/divider/custom_divider.dart';
+import 'package:xcash_app/view/components/text/bottom_sheet_header_text.dart';
+import 'package:xcash_app/view/screens/withdrawals/withdraw_method/widget/withdraw_method_form.dart';
+
+class WithdrawMethod extends StatelessWidget {
+  const WithdrawMethod({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const[
+            BottomSheetHeaderText(text: MyStrings.addWithdrawMethod),
+            BottomSheetCloseButton()
+          ],
+        ),
+
+        const CustomDivider(height: Dimensions.space15),
+
+        const WithdrawMethodForm()
+      ],
+    );
+  }
+}
