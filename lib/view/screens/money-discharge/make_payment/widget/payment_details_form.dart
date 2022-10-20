b669de1fd948b7294b7f0827f7f231637dd30e14
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-20 12:34:06 +0600

    change_text_weight

--- lib/view/screens/money-discharge/make_payment/widget/payment_details_form.dart
+++ lib/view/screens/money-discharge/make_payment/widget/payment_details_form.dart
@@ -100,7 +100,7 @@ class _PaymentDetailsFormState extends State<PaymentDetailsForm> {
             height: 45,
             width: MediaQuery.of(context).size.width,
             backgroundColor: MyColor.primaryColor,
-            child: Text(MyStrings.makePayment, textAlign: TextAlign.center, style: interRegularDefault.copyWith(color: MyColor.colorWhite, fontWeight: FontWeight.w600)),
+            child: Text(MyStrings.makePayment, textAlign: TextAlign.center, style: interRegularDefault.copyWith(color: MyColor.colorWhite, fontWeight: FontWeight.w500)),
           )
         ],
       ),
