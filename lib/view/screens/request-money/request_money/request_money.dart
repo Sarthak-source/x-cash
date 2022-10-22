3f40242240ed08365fb83abc08c83b4b027a5fab
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-22 19:05:24 +0600

    add_request_money_to_bottom_sheet

---
+++ lib/view/screens/request-money/request_money/request_money.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:xcash_app/constants/my_strings.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/view/components/bottom-sheet/bottom_sheet_close_button.dart';
+import 'package:xcash_app/view/components/divider/custom_divider.dart';
+import 'package:xcash_app/view/components/text/bottom_sheet_header_text.dart';
+import 'package:xcash_app/view/screens/request-money/request_money/widget/request_money_form.dart';
+
+class RequestMoney extends StatelessWidget {
+  const RequestMoney({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            BottomSheetHeaderText(text: MyStrings.requestDetails),
+            BottomSheetCloseButton()
+          ],
+        ),
+
+        const CustomDivider(height: Dimensions.space15),
+
+        const RequestMoneyForm()
+      ],
+    );
+  }
+}
+
