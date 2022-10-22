54d0e90cdb8d0dd43c992ad9a34e511f283b75f8
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-22 19:30:02 +0600

    add_to_me_list

--- lib/view/screens/request-money/request_to_me/request_to_me_screen.dart
+++ lib/view/screens/request-money/request_to_me/request_to_me_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:xcash_app/constants/my_strings.dart';
 import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/styles.dart';
 import 'package:xcash_app/view/components/app-bar/custom_app_bar.dart';
 import 'package:xcash_app/view/components/bottom-sheet/custom_bottom_sheet.dart';
 import 'package:xcash_app/view/screens/request-money/request_money/request_money.dart';
+import 'package:xcash_app/view/screens/request-money/request_to_me/widget/to_me_list.dart';
 
 class RequestToMeScreen extends StatefulWidget {
   const RequestToMeScreen({Key? key}) : super(key: key);
@@ -14,6 +17,9 @@ class RequestToMeScreen extends StatefulWidget {
 }
 
 class _RequestToMeScreenState extends State<RequestToMeScreen> {
+
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -47,6 +53,69 @@ class _RequestToMeScreenState extends State<RequestToMeScreen> {
           ],
           changeRoute: () => Get.back(),
         ),
+
+        body: Column(
+          children: [
+
+            Padding(
+              padding: const EdgeInsets.only(top: Dimensions.space20, left: Dimensions.space15, right: Dimensions.space15),
+              child: Container(
+                height: 40,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: MyColor.colorWhite,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  children: [
+                    middleButtons("My Request", 0),
+                    middleButtons("To Me", 1),
+                  ],
+                ),
+              ),
+            ),
+
+            const SizedBox(height: Dimensions.space20),
+
+            selectedIndex == 0 ? const SizedBox() : const Expanded(child: ToMeList()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  middleButtons(String buttonName, int index) {
+    return Expanded(
+      child: GestureDetector(
+          onTap: (){
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          child: index == selectedIndex ? Container(
+
+            width: 150,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(vertical: Dimensions.space10 / 2, horizontal: Dimensions.space10),
+            decoration: BoxDecoration(
+
+                color: index == selectedIndex ? MyColor.primaryColor : MyColor.primaryColor,
+                borderRadius: index == 0 ? const BorderRadius.only(topLeft: Radius.circular(10), bottomLeft: Radius.circular(10)) :
+                const BorderRadius.only(topRight: Radius.circular(10), bottomRight: Radius.circular(10))
+            ),
+            child: Text(buttonName, textAlign: TextAlign.center, style: interRegularSmall.copyWith(color: index == selectedIndex ? MyColor.colorWhite : MyColor.primaryColor)),
+          ) : Container(
+
+            width: 150,
+            alignment: Alignment.center,
+            padding: const EdgeInsets.symmetric(vertical: Dimensions.space10 / 2, horizontal: Dimensions.space10),
+            decoration: BoxDecoration(
+
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(10)
+            ),
+            child: Text(buttonName, textAlign: TextAlign.center, style: interRegularSmall.copyWith(color: MyColor.primaryColor)),
+          )
       ),
     );
   }
