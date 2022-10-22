9b16dcf2b045194cf7ecf777c70dc0bf95044548
Mirza Mahmud Hossan <mirzamahmud.cse.bubt202@gmail.com>
2022-10-22 12:10:40 +0600

    drawer_top_section

---
+++ lib/view/screens/home/Drawer/widget/drawer_top.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/my_images.dart';
+import 'package:xcash_app/core/utils/styles.dart';
+
+class DrawerTop extends StatefulWidget {
+  const DrawerTop({Key? key}) : super(key: key);
+
+  @override
+  State<DrawerTop> createState() => _DrawerTopState();
+}
+
+class _DrawerTopState extends State<DrawerTop> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: Dimensions.space30, left: Dimensions.space15, right: Dimensions.space15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 45, width: 45,
+            decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(image: AssetImage(MyImages.userProfile), fit: BoxFit.fill)
+            ),
+          ),
+          const SizedBox(height: Dimensions.space15),
+
+          Text(
+            "William Anne",
+            style: interRegularDefaultLarge.copyWith(fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: Dimensions.space5),
+          Text(
+            "williamanne123@gmail.com",
+            style: interRegularSmall.copyWith(color: MyColor.primarySubTextColor, fontWeight: FontWeight.w500),
+          )
+        ],
+      ),
+    );
+  }
+}
