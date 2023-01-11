0b81766b0038005f03d9cc38e5a8737995a203c8
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-01-11 19:21:25 +0600

    add_list

--- lib/data/controller/exchange/exchange_money_controller.dart
+++ lib/data/controller/exchange/exchange_money_controller.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:xcash_app/core/utils/my_strings.dart';
 import 'package:xcash_app/data/model/exchange/exchange_money_response_model.dart';
 import 'package:xcash_app/data/model/global/response_model/response_model.dart';
 import 'package:xcash_app/data/repo/exchange/exchange_money_repo.dart';
@@ -40,5 +43,22 @@ class ExchangeMoneyController extends GetxController{
     toWalletList.clear();
 
     amountController.text = "";
+
+    FromWallets fromWallets_ = FromWallets(id: -1, currencyCode: MyStrings.selectOne);
+    fromWalletList.insert(0, fromWallets_);
+
+    ToWallets toWallets_ = ToWallets(id: -1, currencyCode: MyStrings.selectOne);
+    toWalletList.insert(0, toWallets_);
+
+    if(responseModel.statusCode == 200){
+      ExchangeMoneyResponseModel model = ExchangeMoneyResponseModel.fromJson(jsonDecode(responseModel.responseJson));
+
+      if(model.status.toString().toLowerCase() == "success"){
+        List<FromWallets>? tempFromWallets = model.data?.fromWallets;
+        if(tempFromWallets != null || tempFromWallets!.isNotEmpty){
+          fromWalletList.addAll(tempFromWallets);
+        }
+      }
+    }
   }
 }
