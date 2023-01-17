94ad6fabaef109f5fc747dccb1d5de23af9a9060
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-01-17 15:39:38 +0600

    replace_request_money_bottom_sheet_to_screen

--- lib/view/screens/request-money/request_money/request_money.dart
+++ lib/view/screens/request-money/request_money/request_money.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:xcash_app/core/utils/my_strings.dart';
 import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/my_strings.dart';
+import 'package:xcash_app/core/utils/style.dart';
 import 'package:xcash_app/data/controller/request_money/request_money/request_money_controller.dart';
 import 'package:xcash_app/data/repo/request_money/request_money_repo.dart';
 import 'package:xcash_app/data/services/api_service.dart';
-import 'package:xcash_app/view/components/bottom-sheet/bottom_sheet_close_button.dart';
+import 'package:xcash_app/view/components/app-bar/custom_appbar.dart';
+import 'package:xcash_app/view/components/buttons/rounded_button.dart';
+import 'package:xcash_app/view/components/buttons/rounded_loading_button.dart';
 import 'package:xcash_app/view/components/custom_loader/custom_loader.dart';
-import 'package:xcash_app/view/components/divider/custom_divider.dart';
-import 'package:xcash_app/view/components/text/bottom_sheet_header_text.dart';
-import 'package:xcash_app/view/screens/request-money/request_money/widget/request_money_form.dart';
+import 'package:xcash_app/view/components/custom_snackbar.dart';
+import 'package:xcash_app/view/components/text-form-field/custom_amount_text_field.dart';
+import 'package:xcash_app/view/components/text-form-field/custom_text_field.dart';
+import 'package:xcash_app/view/components/text/label_text.dart';
 
-class RequestMoney extends StatefulWidget {
-  const RequestMoney({Key? key}) : super(key: key);
+import '../../../../data/model/request_money/request_money/request_money_response_model.dart';
+
+class RequestMoneyScreen extends StatefulWidget {
+  const RequestMoneyScreen({Key? key}) : super(key: key);
 
   @override
-  State<RequestMoney> createState() => _RequestMoneyState();
+  State<RequestMoneyScreen> createState() => _RequestMoneyScreenState();
 }
 
-class _RequestMoneyState extends State<RequestMoney> {
+class _RequestMoneyScreenState extends State<RequestMoneyScreen> {
 
   @override
   void initState() {
@@ -38,22 +45,113 @@ class _RequestMoneyState extends State<RequestMoney> {
     super.dispose();
   }
 
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return GetBuilder<RequestMoneyController>(
-      builder: (controller) => Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: const [
-              BottomSheetHeaderText(text: MyStrings.requestDetails),
-              BottomSheetCloseButton()
-            ],
+      builder: (controller) => SafeArea(
+        child: Scaffold(
+          backgroundColor: MyColor.getScreenBgColor(),
+          appBar: CustomAppBar(
+            title: MyStrings.requestMoney,
+            isShowBackBtn: true,
+            bgColor: MyColor.getAppBarColor(),
           ),
-          const CustomDivider(space: Dimensions.space15),
-          controller.isLoading ? const CustomLoader() : const RequestMoneyForm()
-        ],
+          body: SingleChildScrollView(
+            padding: Dimensions.screenPaddingHV,
+            child: controller.isLoading ? const Center(
+              child: CustomLoader()
+            ) : Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const LabelText(text: MyStrings.selectWallet),
+                      const SizedBox(height: 8),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        padding: const EdgeInsets.only(left: Dimensions.space15, right: Dimensions.space15,),
+                        decoration: BoxDecoration(
+                            color: MyColor.transparentColor,
+                            borderRadius: BorderRadius.circular(Dimensions.defaultRadius),
+                            border: Border.all(color: MyColor.primaryColor, width: 0.5)
+                        ),
+                        child: DropdownButton<Wallets>(
+                          dropdownColor: MyColor.colorWhite,
+                          value: controller.selectedWallet,
+                          elevation: 8,
+                          icon: const Icon(Icons.keyboard_arrow_down, color: MyColor.primaryColor),
+                          iconDisabledColor: Colors.red,
+                          iconEnabledColor : MyColor.primaryColor,
+                          isExpanded: true,
+                          underline: Container(height: 0, color: MyColor.primaryColor),
+                          onChanged: (Wallets? newValue) {
+                            controller.setWalletMethod(newValue);
+                          },
+                          items: controller.walletList.map((Wallets wallet) {
+                            return DropdownMenuItem<Wallets>(
+                              value: wallet,
+                              child: Text(wallet.currencyCode.toString(), style: regularDefault),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                      const SizedBox(height: Dimensions.space5),
+                      Text("${MyStrings.totalCharge}: ", style: regularExtraSmall.copyWith(color: MyColor.primaryColor))
+                    ],
+                  ),
+                  const SizedBox(height: Dimensions.space15),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomAmountTextField(
+                        controller: controller.amountController,
+                        labelText: MyStrings.amountToRequest,
+                        hintText: MyStrings.amountHint,
+                        onChanged: (value){},
+                        currency: controller.currency,
+                      ),
+                      const SizedBox(height: Dimensions.space5),
+                      Text("Limit: 1.00000000 ~ 100.0000000 USD", style: regularExtraSmall.copyWith(color: MyColor.primaryColor))
+                    ],
+                  ),
+                  const SizedBox(height: Dimensions.space15),
+                  CustomTextField(
+                      needOutlineBorder: true,
+                      controller: controller.requestToController,
+                      labelText: MyStrings.requestTo,
+                      hintText: MyStrings.enterEmailOrUserName,
+                      onChanged: (value){
+                        if(value == null && value.toString().isEmpty){
+                          CustomSnackBar.error(errorList: [MyStrings.enterEmailOrUserName]);
+                        }
+                      }
+                  ),
+                  const SizedBox(height: Dimensions.space15),
+                  CustomTextField(
+                    controller: controller.noteController,
+                    needOutlineBorder: true,
+                    maxLines: 4,
+                    labelText: MyStrings.noteForRecipient,
+                    onChanged: (value){},
+                  ),
+                  const SizedBox(height: Dimensions.space25),
+                  controller.submitLoading ? const RoundedLoadingBtn() : RoundedButton(
+                    press: (){
+                      if(formKey.currentState!.validate()){
+                        controller.submitRequest();
+                      }
+                    },
+                    text: MyStrings.requestNow,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
