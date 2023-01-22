18824d8ee093ffbb2b5b08d7a4ae1659576e5abb
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-01-22 16:00:49 +0600

    change_redeem_voucher_design

--- lib/view/screens/voucher/redeem_voucher/redeem_voucher.dart
+++ lib/view/screens/voucher/redeem_voucher/redeem_voucher.dart
@@ -9,6 +9,7 @@ import 'package:xcash_app/view/components/buttons/rounded_button.dart';
 
 import 'package:xcash_app/view/components/divider/custom_divider.dart';
 import 'package:xcash_app/view/components/text-form-field/custom_amount_text_field.dart';
+import 'package:xcash_app/view/components/text-form-field/custom_text_field.dart';
 import 'package:xcash_app/view/components/text/bottom_sheet_header_text.dart';
 
 class RedeemVoucher extends StatefulWidget {
@@ -27,46 +28,44 @@ class _RedeemVoucherState extends State<RedeemVoucher> {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: const [
-            BottomSheetHeaderText(text: MyStrings.RedeemVoucher),
+            BottomSheetHeaderText(text: MyStrings.redeemVoucher),
             BottomSheetCloseButton()
           ],
         ),
-
         const CustomDivider(),
-
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            RoundedButton(
-                press: (){},
-                text: '',
+            GestureDetector(
+              onTap: (){},
+              child: Container(
+                padding: const EdgeInsets.all(Dimensions.space15),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(color: MyColor.getPrimaryColor(), borderRadius: BorderRadius.circular(Dimensions.defaultRadius)),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
                     Image.asset(MyImages.email, color: MyColor.colorWhite, height: 16, width: 16),
                     const SizedBox(width: Dimensions.space10),
-                    Text("colorRedeem Log", style: regularSmall.copyWith(color: MyColor.colorWhite, fontWeight: FontWeight.w500))
+                    Text("Redeem Log", style: regularSmall.copyWith(color: MyColor.colorWhite, fontWeight: FontWeight.w500))
                   ],
                 ),
+              ),
             )
           ],
         ),
-
         const SizedBox(height: Dimensions.space20),
-
-        CustomAmountTextField(
-            labelText: MyStrings.voucherNumber,
-            hintText: "Enter ${MyStrings.voucherNumber.toLowerCase()}",
-            onChanged: (value){},
-            currency: '',
+        CustomTextField(
+          needOutlineBorder: true,
+          labelText: MyStrings.voucherNumber,
+          hintText: "Enter ${MyStrings.voucherNumber.toLowerCase()}",
+          onChanged: (value){},
         ),
-
         const SizedBox(height: Dimensions.space25),
-
         RoundedButton(
             press: (){},
-            text:MyStrings.colorRedeem
+            text:MyStrings.redeemVoucher
         )
       ],
     );
