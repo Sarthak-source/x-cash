23e2b2c9b0ba61aaaf09db7416589d5065860de3
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-01-23 20:18:47 +0600

    create_tab_widget

--- lib/view/screens/request-money/request_to_me/widget/middle_tab_buttons.dart
+++ lib/view/screens/request-money/request_to_me/widget/middle_tab_buttons.dart
@@ -3,26 +3,27 @@ import 'package:get/get.dart';
 import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
 import 'package:xcash_app/core/utils/style.dart';
+import 'package:xcash_app/data/controller/request_money/request_to_me/my_request_history_controller.dart';
 import 'package:xcash_app/data/controller/voucher/voucher_list_controller.dart';
 
 class MiddleTabButtons extends StatelessWidget {
+
   final String buttonName;
   final bool activeButton;
+  final VoidCallback press;
 
   const MiddleTabButtons({
     Key? key,
     required this.buttonName,
-    required this.activeButton
+    required this.activeButton,
+    required this.press
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<VoucherListController>(
-      builder: (controller) =>  Expanded(
+    return Expanded(
         child: GestureDetector(
-            onTap: (){
-              controller.changeTabSection();
-            },
+            onTap: press,
             child: Container(
               width: 150,
               alignment: Alignment.center,
@@ -34,7 +35,6 @@ class MiddleTabButtons extends StatelessWidget {
               child: Text(buttonName, textAlign: TextAlign.center, style: regularSmall.copyWith(color: activeButton ? MyColor.colorWhite : MyColor.primaryColor)),
             )
         ),
-      ),
-    );
+      );
   }
 }
