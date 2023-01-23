3f8f15544ba7fa9d1b10688816a8fc87fe253a01
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-01-23 15:08:49 +0600

    redeem_voucher_design_and_api_integration_done

--- lib/view/screens/voucher/redeem_voucher/redeem_voucher.dart
+++ lib/view/screens/voucher/redeem_voucher/redeem_voucher.dart
@@ -6,8 +6,14 @@ import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
 import 'package:xcash_app/core/utils/my_images.dart';
 import 'package:xcash_app/core/utils/style.dart';
+import 'package:xcash_app/data/controller/voucher/redeem_voucher_controller.dart';
+import 'package:xcash_app/data/repo/voucher/redeem_voucher_repo.dart';
+import 'package:xcash_app/data/services/api_service.dart';
 import 'package:xcash_app/view/components/bottom-sheet/bottom_sheet_close_button.dart';
 import 'package:xcash_app/view/components/buttons/rounded_button.dart';
+import 'package:xcash_app/view/components/buttons/rounded_loading_button.dart';
+import 'package:xcash_app/view/components/custom_loader/custom_loader.dart';
+import 'package:xcash_app/view/components/custom_snackbar.dart';
 
 import 'package:xcash_app/view/components/divider/custom_divider.dart';
 import 'package:xcash_app/view/components/text-form-field/custom_amount_text_field.dart';
@@ -22,56 +28,93 @@ class RedeemVoucher extends StatefulWidget {
 }
 
 class _RedeemVoucherState extends State<RedeemVoucher> {
+
+  @override
+  void initState() {
+    Get.put(ApiClient(sharedPreferences: Get.find()));
+    Get.put(RedeemVoucherRepo(apiClient: Get.find()));
+    final controller = Get.put(RedeemVoucherController(redeemVoucherRepo: Get.find()));
+
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      controller.codeController.text = "";
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
-            BottomSheetHeaderText(text: MyStrings.redeemVoucher),
-            BottomSheetCloseButton()
-          ],
-        ),
-        const CustomDivider(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            GestureDetector(
-              onTap: (){
-                Get.toNamed(RouteHelper.redeemLogScreen);
-              },
-              child: Container(
-                padding: const EdgeInsets.all(Dimensions.space15),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(color: MyColor.getPrimaryColor(), borderRadius: BorderRadius.circular(Dimensions.defaultRadius)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(MyImages.email, color: MyColor.colorWhite, height: 16, width: 16),
-                    const SizedBox(width: Dimensions.space10),
-                    Text(MyStrings.redeemLog, style: regularSmall.copyWith(color: MyColor.colorWhite, fontWeight: FontWeight.w500))
-                  ],
+    return GetBuilder<RedeemVoucherController>(
+      builder: (controller) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              BottomSheetHeaderText(text: MyStrings.redeemVoucher),
+              BottomSheetCloseButton()
+            ],
+          ),
+          const CustomDivider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              GestureDetector(
+                onTap: (){
+                  Get.toNamed(RouteHelper.redeemLogScreen);
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(Dimensions.space15),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(color: MyColor.getPrimaryColor(), borderRadius: BorderRadius.circular(Dimensions.defaultRadius)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Image.asset(MyImages.email, color: MyColor.colorWhite, height: 16, width: 16),
+                      const SizedBox(width: Dimensions.space10),
+                      Text(MyStrings.redeemLog, style: regularSmall.copyWith(color: MyColor.colorWhite, fontWeight: FontWeight.w500))
+                    ],
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
-        const SizedBox(height: Dimensions.space20),
-        CustomTextField(
-          needOutlineBorder: true,
-          labelText: MyStrings.voucherNumber,
-          hintText: "Enter ${MyStrings.voucherNumber.toLowerCase()}",
-          onChanged: (value){},
-        ),
-        const SizedBox(height: Dimensions.space25),
-        RoundedButton(
-            press: (){},
-            text:MyStrings.redeemVoucher
-        )
-      ],
+              )
+            ],
+          ),
+          const SizedBox(height: Dimensions.space20),
+          Form(
+            key: formKey,
+            child: CustomTextField(
+              needOutlineBorder: true,
+              labelText: MyStrings.voucherCode,
+              hintText: MyStrings.enterVoucherCode,
+              controller: controller.codeController,
+              onChanged: (value){},
+              validator: (value){
+                if(value.toString().isEmpty){
+                  return MyStrings.errorMsgVoucherCode;
+                }
+                return null;
+              },
+            ),
+          ),
+          const SizedBox(height: Dimensions.space25),
+          controller.submitLoading ? const RoundedLoadingBtn() : RoundedButton(
+              press: (){
+                if(formKey.currentState!.validate()){
+                  controller.submitRedeemVoucher();
+                }
+              },
+              text:MyStrings.redeemVoucher
+          )
+        ],
+      ),
     );
   }
 }
