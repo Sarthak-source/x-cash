ed5cd73a53dafb9ee18ff356c5a39d69eb5a6284
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-01-26 16:59:45 +0600

    change_withdraw_money_screen_design_and_integrate_api_data

--- lib/view/screens/withdrawals/withdraw_money/withdraw_money_screen.dart
+++ lib/view/screens/withdrawals/withdraw_money/withdraw_money_screen.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:xcash_app/core/utils/my_strings.dart';
+import 'package:xcash_app/core/helper/string_format_helper.dart';
 import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
 import 'package:xcash_app/core/utils/my_images.dart';
+import 'package:xcash_app/core/utils/my_strings.dart';
 import 'package:xcash_app/core/utils/style.dart';
-import 'package:xcash_app/view/components/app-bar/custom_appbar.dart';
-
+import 'package:xcash_app/data/controller/withdraw/withdraw_money_controller.dart';
+import 'package:xcash_app/data/repo/withdraw/withdraw_money_repo.dart';
+import 'package:xcash_app/data/services/api_service.dart';
+import 'package:xcash_app/view/components/bottom-sheet/bottom_sheet_close_button.dart';
 import 'package:xcash_app/view/components/bottom-sheet/custom_bottom_sheet.dart';
+import 'package:xcash_app/view/components/buttons/rounded_button.dart';
+import 'package:xcash_app/view/components/buttons/rounded_loading_button.dart';
 import 'package:xcash_app/view/components/card/custom_card.dart';
+import 'package:xcash_app/view/components/custom_loader/custom_loader.dart';
+import 'package:xcash_app/view/components/custom_no_data_found_class.dart';
+import 'package:xcash_app/view/components/divider/custom_divider.dart';
+import 'package:xcash_app/view/components/text-form-field/custom_amount_text_field.dart';
+import 'package:xcash_app/view/components/text/bottom_sheet_header_text.dart';
 import 'package:xcash_app/view/screens/withdrawals/withdraw_method/withdraw_method.dart';
-import 'package:xcash_app/view/screens/withdrawals/withdraw_money/widget/withdraw_money_bottom_sheet.dart';
+import 'package:xcash_app/view/screens/withdrawals/withdraw_money/widget/withdraw_money_status.dart';
 
 class WithdrawMoneyScreen extends StatefulWidget {
   const WithdrawMoneyScreen({Key? key}) : super(key: key);
@@ -21,95 +31,209 @@ class WithdrawMoneyScreen extends StatefulWidget {
 
 class _WithdrawMoneyScreenState extends State<WithdrawMoneyScreen> {
 
-  List<Map<String, dynamic>> data = [
-    {"bankImage" : MyImages.basic, "bankName" : MyStrings.basic, "amount" : MyStrings.minMaxAmount, "charge" : 2.00, "rate" : 1.00},
-    {"bankImage" : MyImages.payoneer, "bankName" : MyStrings.payoneer, "amount" : MyStrings.minMaxAmount, "charge" : 2.00, "rate" : 1.00},
-    {"bankImage" : MyImages.paypal, "bankName" : MyStrings.paypal, "amount" : MyStrings.minMaxAmount, "charge" : 2.00, "rate" : 1.00},
-  ];
+  final ScrollController scrollController = ScrollController();
+  void scrollListener(){
+    if(scrollController.position.pixels == scrollController.position.maxScrollExtent){
+      if(Get.find<WithdrawMoneyController>().hasNext()){
+        Get.find<WithdrawMoneyController>().loadData();
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    Get.put(ApiClient(sharedPreferences: Get.find()));
+    Get.put(WithdrawMoneyRepo(apiClient: Get.find()));
+    final controller = Get.put(WithdrawMoneyController(withdrawMoneyRepo: Get.find()));
+
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      controller.initialData();
+      scrollController.addListener(scrollListener);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: MyColor.screenBgColor,
-        appBar: AppBar(
-          elevation: 0,
-          title: Text(MyStrings.withdrawMoney, style: regularDefault.copyWith(color: MyColor.getAppBarContentColor())),
-          backgroundColor: MyColor.getAppBarColor(),
-          leading: IconButton(onPressed: () => Get.back(), icon: Icon(Icons.arrow_back, color: MyColor.getAppBarContentColor(), size: 20)),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: Dimensions.space15),
-              child: GestureDetector(
-                onTap: (){
-                  CustomBottomSheet(
-                    child: const WithdrawMethod()
-                  ).customBottomSheet(context);
-                },
-                child: Container(
-                  height: 30, width: 30,
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(color: MyColor.colorWhite, shape: BoxShape.circle),
-                  child: Icon(Icons.add, color: MyColor.getPrimaryColor(), size: 15),
+    return GetBuilder<WithdrawMoneyController>(
+      builder: (controller) => SafeArea(
+        child: Scaffold(
+          backgroundColor: MyColor.screenBgColor,
+          appBar: AppBar(
+            elevation: 0,
+            title: Text(MyStrings.withdrawMoney, style: regularDefault.copyWith(color: MyColor.getAppBarContentColor())),
+            backgroundColor: MyColor.getAppBarColor(),
+            leading: IconButton(onPressed: () => Get.back(), icon: Icon(Icons.arrow_back, color: MyColor.getAppBarContentColor(), size: 20)),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: Dimensions.space15),
+                child: GestureDetector(
+                  onTap: (){
+                    CustomBottomSheet(
+                        child: const WithdrawMethod()
+                    ).customBottomSheet(context);
+                  },
+                  child: Container(
+                    height: 30, width: 30,
+                    alignment: Alignment.center,
+                    decoration: const BoxDecoration(color: MyColor.colorWhite, shape: BoxShape.circle),
+                    child: Icon(Icons.add, color: MyColor.getPrimaryColor(), size: 15),
+                  ),
                 ),
-              ),
-            )
-          ],
-        ),
-        body: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(vertical: Dimensions.space20, horizontal: Dimensions.space15),
-          child: ListView.separated(
-            shrinkWrap: true,
-            padding: EdgeInsets.zero,
-            scrollDirection: Axis.vertical,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: data.length,
-            separatorBuilder: (context, index) => const SizedBox(height: Dimensions.space10 + 2),
-            itemBuilder: (context, index) => CustomCard(
-              paddingTop: Dimensions.space15,
-              paddingBottom: Dimensions.space15,
-              isPress: true,
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                children: [
-                  Container(
-                    height: 60, width: 60,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      image: DecorationImage(
-                        image: AssetImage(data[index]["bankImage"]),
-                        fit: BoxFit.cover
-                      )
+              )
+            ],
+          ),
+          body: controller.isLoading ? const CustomLoader() : Padding(
+            padding: const EdgeInsets.only(top: Dimensions.space20, left: Dimensions.space15, right: Dimensions.space15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: controller.withdrawMoneyList.isEmpty ? const Center(
+                    child: NoDataOrInternetScreen(),
+                  ) : SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    child: ListView.separated(
+                        shrinkWrap: true,
+                        padding: EdgeInsets.zero,
+                        scrollDirection: Axis.vertical,
+                        physics: const BouncingScrollPhysics(),
+                        itemCount: controller.withdrawMoneyList.length + 1,
+                        separatorBuilder: (context, index) => const SizedBox(height: Dimensions.space10 + 2),
+                        itemBuilder: (context, index){
+                          if(controller.withdrawMoneyList.length == index){
+                            return controller.hasNext() ? const CustomLoader(isPagination: true) : const SizedBox();
+                          }
+                          return CustomCard(
+                            paddingTop: Dimensions.space15,
+                            paddingBottom: Dimensions.space15,
+                            isPress: true,
+                            width: MediaQuery.of(context).size.width,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Row(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Container(
+                                          height: 35, width: 35,
+                                          alignment: Alignment.center,
+                                          decoration: BoxDecoration(color: MyColor.getPrimaryColor().withOpacity(0.2), shape: BoxShape.circle),
+                                          child: Image.asset(MyImages.withdrawMoney, color: MyColor.getPrimaryColor(), height: 17, width: 17),
+                                        ),
+                                        const SizedBox(width: Dimensions.space15),
+                                        Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                                controller.withdrawMoneyList[index].name ?? "",
+                                                style: regularDefault.copyWith(color: MyColor.getTextColor(), fontWeight: FontWeight.w600)
+                                            ),
+                                            const SizedBox(height: Dimensions.space5),
+                                            Text(
+                                                "${controller.withdrawMoneyList[index].withdrawMethod?.name ?? ""} - ${controller.withdrawMoneyList[index].currency?.currencyCode ?? ""}",
+                                                style: regularSmall.copyWith(color: MyColor.getPrimaryColor())
+                                            ),
+                                            const SizedBox(height: Dimensions.space10),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                    WithdrawMoneyStatus(status: controller.withdrawMoneyList[index].withdrawMethod?.status ?? "")
+                                  ],
+                                ),
+                                const CustomDivider(space: Dimensions.space10),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          MyStrings.limit,
+                                          style: regularSmall.copyWith(color: MyColor.getTextColor().withOpacity(0.6)),
+                                        ),
+                                        const SizedBox(height: Dimensions.space5),
+                                        Text(
+                                          "${Converter.twoDecimalPlaceFixedWithoutRounding(controller.withdrawMoneyList[index].withdrawMethod?.withdrawMinLimit ?? "")} ~ "
+                                          "${Converter.twoDecimalPlaceFixedWithoutRounding(controller.withdrawMoneyList[index].withdrawMethod?.withdrawMaxLimit ?? "")} "
+                                          "${controller.withdrawMoneyList[index].currency?.currencyCode ?? ""}",
+                                          style: regularSmall.copyWith(color: MyColor.getTextColor(), fontWeight: FontWeight.w600)
+                                        ),
+                                      ],
+                                    ),
+                                    const SizedBox(width: Dimensions.space10),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.end,
+                                      children: [
+                                        Text(
+                                          MyStrings.charge,
+                                          style: regularSmall.copyWith(color: MyColor.getTextColor().withOpacity(0.6)),
+                                        ),
+                                        const SizedBox(height: Dimensions.space5),
+                                        controller.withdrawMoneyList[index].withdrawMethod?.id == 4 ? Text(
+                                            "${Converter.twoDecimalPlaceFixedWithoutRounding(controller.withdrawMoneyList[index].withdrawMethod?.withdrawMinLimit ?? "")} "
+                                                "${controller.withdrawMoneyList[index].currency?.currencyCode ?? ""} + "
+                                                "${Converter.twoDecimalPlaceFixedWithoutRounding(controller.withdrawMoneyList[index].withdrawMethod?.percentCharge ?? "")}%",
+                                            style: regularSmall.copyWith(color: MyColor.getTextColor(), fontWeight: FontWeight.w600)
+                                        ) : Text(
+                                            "${Converter.twoDecimalPlaceFixedWithoutRounding(controller.withdrawMoneyList[index].withdrawMethod?.withdrawMaxLimit ?? "")} "
+                                                "${controller.withdrawMoneyList[index].currency?.currencyCode ?? ""} + "
+                                                "${Converter.twoDecimalPlaceFixedWithoutRounding(controller.withdrawMoneyList[index].withdrawMethod?.percentCharge ?? "")}%",
+                                            style: regularSmall.copyWith(color: MyColor.getTextColor(), fontWeight: FontWeight.w600)
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ],
+                            ),
+                            onPressed: (){
+                              CustomBottomSheet(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Row(
+                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                        children: const [
+                                          BottomSheetHeaderText(text: MyStrings.withdrawMoney),
+                                          BottomSheetCloseButton()
+                                        ],
+                                      ),
+                                      const CustomDivider(space: Dimensions.space15),
+                                      CustomAmountTextField(
+                                          labelText: MyStrings.amount,
+                                          hintText: MyStrings.amountHint,
+                                          currency: controller.withdrawMoneyList[index].currency?.currencyCode ?? "",
+                                          controller: controller.amountController,
+                                          onChanged: (value){}
+                                      ),
+                                      const SizedBox(height: Dimensions.space25),
+                                      controller.submitLoading ? const RoundedLoadingBtn() : RoundedButton(
+                                        text: MyStrings.submit,
+                                        press: (){
+                                          controller.submitData(
+                                              methodId: controller.withdrawMoneyList[index].withdrawMethod?.id.toString() ?? "",
+                                              userMethodId: controller.withdrawMoneyList[index].id.toString()
+                                          );
+                                        },
+                                      )
+                                    ],
+                                  )
+                              ).customBottomSheet(context);
+                            },
+                          );
+                        }
                     ),
                   ),
-
-                  const SizedBox(width: Dimensions.space15),
-
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(data[index]["bankName"], style: regularDefault.copyWith(fontWeight: FontWeight.w500)),
-                      const SizedBox(height: Dimensions.space5),
-
-                      Text(data[index]["amount"], style: regularSmall.copyWith(color: MyColor.contentTextColor)),
-
-                      const SizedBox(height: Dimensions.space10),
-                      Text("Charge : ${data[index]["charge"].toString()} USD + ${data[index]["rate"].toString()}%", style: regularSmall.copyWith(color: MyColor.contentTextColor)),
-                    ],
-                  )
-                ],
-              ),
-              onPressed: (){
-                WithdrawMoneyBottomSheet.bottomSheet(
-                    context,
-                    "${data[index]["bankImage"]}",
-                    "${data[index]["bankName"]}",
-                    "${data[index]["amount"]}",
-                    "${data[index]["charge"]}",
-                    "${data[index]["rate"]}"
-                );
-              },
-            )
+                ),
+              ],
+            ),
           ),
         ),
       ),
