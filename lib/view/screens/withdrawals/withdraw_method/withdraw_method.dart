fa2fcdbb99329b97bd5e6522cf9033d2f732d329
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-01-28 16:08:32 +0600

    create_withdraw_method_screen

--- lib/view/screens/withdrawals/withdraw_method/withdraw_method.dart
+++ lib/view/screens/withdrawals/withdraw_method/withdraw_method.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
 import 'package:xcash_app/core/utils/my_strings.dart';
 import 'package:xcash_app/core/utils/dimensions.dart';
-import 'package:xcash_app/view/components/bottom-sheet/bottom_sheet_close_button.dart';
-import 'package:xcash_app/view/components/divider/custom_divider.dart';
-import 'package:xcash_app/view/components/text/bottom_sheet_header_text.dart';
+import 'package:xcash_app/view/components/app-bar/custom_appbar.dart';
 import 'package:xcash_app/view/screens/withdrawals/withdraw_method/widget/withdraw_method_form.dart';
 
-class WithdrawMethod extends StatelessWidget {
-  const WithdrawMethod({Key? key}) : super(key: key);
+class WithdrawMethodScreen extends StatelessWidget {
+  const WithdrawMethodScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const[
-            BottomSheetHeaderText(text: MyStrings.addWithdrawMethod),
-            BottomSheetCloseButton()
-          ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: MyColor.getScreenBgColor(),
+        appBar: CustomAppBar(
+          title: MyStrings.withdrawMethod,
+          bgColor: MyColor.getAppBarColor(),
         ),
-
-        const CustomDivider(space: Dimensions.space15),
-
-        const WithdrawMethodForm()
-      ],
+        body: SingleChildScrollView(
+          padding: Dimensions.screenPaddingHV,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              WithdrawMethodForm()
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
