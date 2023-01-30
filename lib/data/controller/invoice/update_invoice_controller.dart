89111d452077f687979392ed56a6ede18a972291
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-01-30 19:01:29 +0600

    remove_unused_import

--- lib/data/controller/invoice/update_invoice_controller.dart
+++ lib/data/controller/invoice/update_invoice_controller.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:xcash_app/core/helper/string_format_helper.dart';
-import 'package:xcash_app/core/route/route.dart';
 import 'package:xcash_app/core/utils/my_strings.dart';
 import 'package:xcash_app/data/model/authorization/authorization_response_model.dart';
 import 'package:xcash_app/data/model/global/response_model/response_model.dart';
