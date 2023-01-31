d72552163a3b4411e657a7e8587f83654271e6c7
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-01-31 12:50:35 +0600

    create_add_money_screen_bottom_sheet

--- lib/view/screens/withdrawals/withdraw_money/widget/withdraw_money_bottom_sheet.dart
+++ lib/view/screens/withdrawals/withdraw_money/widget/withdraw_money_bottom_sheet.dart
@@ -1,86 +1,65 @@
 import 'package:flutter/material.dart';
-import 'package:xcash_app/core/utils/my_strings.dart';
+import 'package:get/get.dart';
 import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
-import 'package:xcash_app/core/utils/style.dart';
+import 'package:xcash_app/core/utils/my_strings.dart';
+import 'package:xcash_app/data/controller/withdraw/withdraw_money_controller.dart';
 import 'package:xcash_app/view/components/bottom-sheet/bottom_sheet_close_button.dart';
+import 'package:xcash_app/view/components/buttons/rounded_button.dart';
+import 'package:xcash_app/view/components/buttons/rounded_loading_button.dart';
 import 'package:xcash_app/view/components/divider/custom_divider.dart';
+import 'package:xcash_app/view/components/text-form-field/custom_amount_text_field.dart';
 import 'package:xcash_app/view/components/text/bottom_sheet_header_text.dart';
-import 'package:xcash_app/view/screens/withdrawals/withdraw_money/widget/withdraw_money_form.dart';
 
 class WithdrawMoneyBottomSheet{
 
-  static void bottomSheet(BuildContext context, String bankImage, String bankName, String amount, String charge, String rate){
+  static void bottomSheet(BuildContext context, {required int index}){
 
     showModalBottomSheet(
         isScrollControlled: true,
         backgroundColor: MyColor.transparentColor,
         context: context,
-        builder: (BuildContext context) => SingleChildScrollView(
-          physics: const ClampingScrollPhysics(),
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: Dimensions.space15, vertical: Dimensions.space20),
-            width: MediaQuery.of(context).size.width,
-            decoration: const BoxDecoration(
-                color: MyColor.colorWhite,
-                borderRadius: BorderRadius.vertical(top: Radius.circular(15))
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    BottomSheetHeaderText(text: MyStrings.requestDetails),
-                    BottomSheetCloseButton()
-                  ],
-                ),
-
-                const CustomDivider(),
-
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.all(Dimensions.space15),
-                  decoration: BoxDecoration(
-                    color: MyColor.colorWhite,
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(color: MyColor.colorGrey.withOpacity(0.6), width: 0.5)
-                  ),
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 60, width: 60,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            image: DecorationImage(
-                                image: AssetImage(bankImage),
-                                fit: BoxFit.cover
-                            )
-                        ),
-                      ),
-
-                      const SizedBox(width: Dimensions.space15),
-
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(bankName, style: regularDefault.copyWith(fontWeight: FontWeight.w500)),
-                          const SizedBox(height: Dimensions.space5),
-
-                          Text(amount, style: regularSmall.copyWith(color: MyColor.contentTextColor)),
-
-                          const SizedBox(height: Dimensions.space10),
-                          Text("Charge : $charge USD + $rate%", style: regularSmall.copyWith(color: MyColor.contentTextColor)),
-                        ],
-                      )
+        builder: (BuildContext context) => GetBuilder<WithdrawMoneyController>(
+          builder: (controller) => SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: Dimensions.space15, vertical: Dimensions.space20),
+              width: MediaQuery.of(context).size.width,
+              decoration: const BoxDecoration(
+                  color: MyColor.colorWhite,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(15))
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      BottomSheetHeaderText(text: MyStrings.withdrawMoney),
+                      BottomSheetCloseButton()
                     ],
                   ),
-                ),
-
-                const SizedBox(height: Dimensions.space30),
-
-                const WithdrawMoneyForm()
-              ],
+                  const CustomDivider(space: Dimensions.space15),
+                  CustomAmountTextField(
+                      labelText: MyStrings.amount,
+                      hintText: MyStrings.amountHint,
+                      currency: controller.withdrawMoneyList[index].currency?.currencyCode ?? "",
+                      controller: controller.amountController,
+                      onChanged: (value){}
+                  ),
+                  const SizedBox(height: Dimensions.space25),
+                  controller.submitLoading ? const RoundedLoadingBtn() : RoundedButton(
+                    text: MyStrings.submit,
+                    press: (){
+                      controller.submitData(
+                          methodName: controller.withdrawMoneyList[index].withdrawMethod?.name ?? "",
+                          methodId: controller.withdrawMoneyList[index].withdrawMethod?.id.toString() ?? "",
+                          userMethodId: controller.withdrawMoneyList[index].id.toString()
+                      );
+                    },
+                  )
+                ],
+              ),
             ),
           ),
         )
