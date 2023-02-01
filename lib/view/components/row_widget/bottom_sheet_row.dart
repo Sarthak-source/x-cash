9f875c2c5791e8a10dc787ee0f3c48c377ed4c07
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-02-01 13:25:49 +0600

    create_new_bottom_sheet_widget

---
+++ lib/view/components/row_widget/bottom_sheet_row.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/style.dart';
+
+class BottomSheetRow extends StatelessWidget {
+
+  final String header;
+  final String body;
+
+  const BottomSheetRow({Key? key, required this.header,required this.body}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(header, style: regularDefault.copyWith(color: MyColor.getTextColor().withOpacity(0.6)),overflow: TextOverflow.ellipsis,),
+        Text(body,style: regularDefault.copyWith(color: MyColor.getTextColor(), fontWeight: FontWeight.w500), overflow: TextOverflow.ellipsis)
+      ],
+    );
+  }
+}
