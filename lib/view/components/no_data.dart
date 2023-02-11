4bc68547a9fb7ab3bd22d210c984a1da1707cb29
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-02-11 17:37:13 +0600

    add_new_widget

---
+++ lib/view/components/no_data.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:xcash_app/core/utils/dimensions.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/my_images.dart';
+import 'package:xcash_app/core/utils/my_strings.dart';
+import 'package:xcash_app/core/utils/style.dart';
+
+class NoDataWidget extends StatelessWidget {
+  const NoDataWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(MyImages.noDataImage, height: 70, width: 70, color: MyColor.primaryColor),
+          const SizedBox(height: Dimensions.space15),
+          Text(
+            MyStrings.noDataFound,
+            style: regularLarge.copyWith(color: MyColor.getTextColor().withOpacity(0.6)),
+          )
+        ],
+      ),
+    );
+  }
+}
