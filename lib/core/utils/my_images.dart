546b99b5cc9da6c4e9829261c371eed3c2d3c727
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-02-11 17:35:59 +0600

    add_new_images

--- lib/core/utils/my_images.dart
+++ lib/core/utils/my_images.dart
@@ -11,7 +11,8 @@ class MyImages{
   static const String menuAddMoneyHistory = "assets/images/menu_add_money_history.svg";
   static const String menuInvoice = "assets/images/menu_invoice.svg";
   static const String menuMakePayment = "assets/images/menu_make_payment.svg";
-  static const String menuRedeemLog = "assets/images/menu_redeem_log.svg";
+  static const String menuRedeemVoucher = "assets/images/menu_redeem_voucher.svg";
+  static const String menuRedeemLog = "assets/images/menu_redeem_log.png";
   static const String menuTransaction = "assets/images/menu_transaction.svg";
   static const String menuTransfer = "assets/images/menu_transfer.svg";
   static const String menuVoucher = "assets/images/menu_voucher.svg";
@@ -20,6 +21,7 @@ class MyImages{
   static const String withdrawMethod = "assets/images/withdraw_method.png";
   static const String requestMoney = "assets/images/request_money.svg";
   static const String menuRequestToMe = "assets/images/request_to_me.svg";
+  static const String menuRequestToMe1 = "assets/images/request_to_me.png";
   static const String transaction = "assets/images/transaction.svg";
   static const String transfer = "assets/images/transfer.svg";
   static const String voucher = "assets/images/voucher.svg";
@@ -58,6 +60,7 @@ class MyImages{
   static const String noDataImage = "assets/images/no_data.svg";
   static const String errorImage = "assets/images/Error.svg";
   static const String successImage = "assets/images/success_icon.png";
+  static const String otpImage = "assets/images/otp.png";
 
   static const String usDollar = "assets/icons/coins/dollar.png";
   static const String bdTaka = "assets/icons/coins/taka.png";
