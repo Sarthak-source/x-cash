e8a99ea4b59aa46d5e47e5780d8b7cc09f2ea7c2
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-02-14 18:21:16 +0600

    change_design

--- lib/view/screens/bottom_nav_section/home/widget/insight_section.dart
+++ lib/view/screens/bottom_nav_section/home/widget/insight_section.dart
@@ -3,10 +3,12 @@ import 'package:get/get.dart';
 import 'package:xcash_app/core/route/route.dart';
 import 'package:xcash_app/core/utils/dimensions.dart';
 import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/my_images.dart';
 import 'package:xcash_app/core/utils/my_strings.dart';
 import 'package:xcash_app/core/utils/style.dart';
 import 'package:xcash_app/data/controller/home/home_controller.dart';
 import 'package:xcash_app/view/components/divider/custom_divider.dart';
+import 'package:xcash_app/view/components/image/circle_shape_image.dart';
 
 class InsightSection extends StatelessWidget {
   const InsightSection({Key? key}) : super(key: key);
@@ -29,7 +31,7 @@ class InsightSection extends StatelessWidget {
                   style: regularDefault.copyWith(color: MyColor.getTextColor(), fontWeight: FontWeight.w500),
                 ),
                 Text(
-                  "( Last 7 days )",
+                  MyStrings.selectInsight,
                   textAlign: TextAlign.center,
                   style: regularSmall.copyWith(color: MyColor.getPrimaryColor()),
                 )
@@ -39,125 +41,64 @@ class InsightSection extends StatelessWidget {
             Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.symmetric(vertical: Dimensions.space12, horizontal: Dimensions.space15),
-                  decoration: BoxDecoration(
-                      color: MyColor.getCardBgColor(),
-                      border: Border.all(width: 0.5, color: MyColor.colorGrey.withOpacity(0.2)),
-                      borderRadius: BorderRadius.circular(Dimensions.defaultRadius)),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(MyStrings.moneyIn, style: regularDefault.copyWith(fontWeight: FontWeight.w600)),
-                              const SizedBox(height: Dimensions.space10),
-                              Text(controller.totalMoneyIn, style: regularLarge.copyWith(fontWeight: FontWeight.w600)),
-                            ],
-                          ),
-                          GestureDetector(
-                            onTap: (){},
-                            child: Text(
-                              MyStrings.totalReceived.tr,
-                              textAlign: TextAlign.center,
-                              style: regularDefault.copyWith(decoration: TextDecoration.underline, color: MyColor.getTextColor().withOpacity(0.5)),
-                            ),
-                          ),
-                        ],
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const CircleShapeImage(image: MyImages.inMoney),
+                        const SizedBox(width: Dimensions.space8),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(MyStrings.moneyIn, style: regularDefault.copyWith(fontWeight: FontWeight.w600)),
+                            const SizedBox(height: Dimensions.space10),
+                            Text(controller.totalMoneyIn, style: regularLarge.copyWith(fontWeight: FontWeight.w600)),
+                          ],
+                        ),
+                      ],
+                    ),
+                    GestureDetector(
+                      onTap: (){},
+                      child: Container(
+                        height: 30, width: 30,
+                        alignment: Alignment.center,
+                        decoration: const BoxDecoration(color: MyColor.transparentColor, shape: BoxShape.circle),
+                        child: Image.asset(MyImages.dots, color: MyColor.colorBlack, height: 15, width: 15),
                       ),
-                      const SizedBox(height: Dimensions.space20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          GestureDetector(
-                            onTap: (){
-                              Get.toNamed(RouteHelper.requestMoneyScreen);
-                            },
-                            child: Text(
-                              MyStrings.requestMoney.tr,
-                              style: regularDefault.copyWith(color: MyColor.getPrimaryColor()),
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: (){
-                              Get.toNamed(RouteHelper.transactionHistoryScreen);
-                            },
-                            child: Text(
-                              MyStrings.viewTransactions.tr,
-                              style: regularDefault.copyWith(color: MyColor.getPrimaryColor()),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
-                const SizedBox(height: Dimensions.space15),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  padding: const EdgeInsets.symmetric(vertical: Dimensions.space12, horizontal: Dimensions.space15),
-                  decoration: BoxDecoration(
-                    color: MyColor.getCardBgColor(),
-                    border: Border.all(width: 0.5, color: MyColor.colorGrey.withOpacity(0.2)),
-                    borderRadius: BorderRadius.circular(Dimensions.defaultRadius)
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(MyStrings.moneyOut.tr, style: regularDefault.copyWith(fontWeight: FontWeight.w600)),
-                              const SizedBox(height: Dimensions.space10),
-                              Text(controller.totalMoneyOut, style: regularLarge.copyWith(fontWeight: FontWeight.w600)),
-                            ],
-                          ),
-                          GestureDetector(
-                            onTap: (){},
-                            child: Text(
-                              MyStrings.totalSpent.tr,
-                              textAlign: TextAlign.center,
-                              style: regularDefault.copyWith(decoration: TextDecoration.underline, color: MyColor.getTextColor().withOpacity(0.5)),
-                            ),
-                          ),
-                        ],
+                const CustomDivider(space: Dimensions.space15),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const CircleShapeImage(image: MyImages.outMoney),
+                        const SizedBox(width: Dimensions.space8),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(MyStrings.moneyOut, style: regularDefault.copyWith(fontWeight: FontWeight.w600)),
+                            const SizedBox(height: Dimensions.space10),
+                            Text(controller.totalMoneyOut, style: regularLarge.copyWith(fontWeight: FontWeight.w600)),
+                          ],
+                        ),
+                      ],
+                    ),
+                    GestureDetector(
+                      onTap: (){},
+                      child: Container(
+                          height: 30, width: 30,
+                        alignment: Alignment.center,
+                        decoration: const BoxDecoration(color: MyColor.transparentColor, shape: BoxShape.circle),
+                        child: Image.asset(MyImages.dots, color: MyColor.colorBlack, height: 15, width: 15),
                       ),
-                      const SizedBox(height: Dimensions.space20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          GestureDetector(
-                            onTap: (){
-                              Get.toNamed(RouteHelper.transferMoneyScreen);
-                            },
-                            child: Text(
-                              MyStrings.sendMoney.tr,
-                              style: regularDefault.copyWith(color: MyColor.getPrimaryColor()),
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: (){
-                              Get.toNamed(RouteHelper.transactionHistoryScreen);
-                            },
-                            child: Text(
-                              MyStrings.viewTransactions.tr,
-                              style: regularDefault.copyWith(color: MyColor.getPrimaryColor()),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
+                    )
+                  ],
                 ),
               ],
             )
