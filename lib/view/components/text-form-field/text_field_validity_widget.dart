3349ebdabd114eafa0c6d61a2a2263478aa48deb
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-02-14 13:30:48 +0600

    create_new_widget_for_text_field

---
+++ lib/view/components/text-form-field/text_field_validity_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:xcash_app/core/utils/my_color.dart';
+import 'package:xcash_app/core/utils/style.dart';
+
+class TextFieldValidityWidget extends StatelessWidget {
+
+  final bool? isVisible;
+  final String validMsg;
+  final String invalidMsg;
+
+  const TextFieldValidityWidget({
+    Key? key,
+    this.isVisible,
+    required this.validMsg,
+    required this.invalidMsg
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Visibility(
+      visible: isVisible != null,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          isVisible == true ? Text(
+            validMsg,
+            style: regularSmall.copyWith(color: MyColor.colorGreen, fontWeight: FontWeight.w500),
+          ) : Text(
+            invalidMsg,
+            style: regularSmall.copyWith(color: MyColor.colorRed, fontWeight: FontWeight.w500),
+          ),
+        ],
+      )
+    );
+  }
+}
