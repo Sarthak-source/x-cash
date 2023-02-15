1b8593c10ffbac103b42eb3698108e565dffb680
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2023-02-15 16:46:25 +0600

    change_add_money_history_filter_widget_design

--- lib/view/screens/add-money/add_money_history/widget/add_money_history_filter_widget.dart
+++ lib/view/screens/add-money/add_money_history/widget/add_money_history_filter_widget.dart
@@ -12,59 +12,65 @@ class AddMoneyHistoryFilterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<AddMoneyHistoryController>(
-      builder: (controller) => Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(MyStrings.transactionNo.tr, style: regularSmall.copyWith(color: MyColor.getLabelTextColor(), fontWeight: FontWeight.w500)),
-                const SizedBox(height: Dimensions.space5),
-                SizedBox(
-                  height: 45,
-                  width: MediaQuery.of(context).size.width,
-                  child: TextFormField(
-                    cursorColor: MyColor.primaryColor,
-                    style: regularSmall.copyWith(color: MyColor.colorBlack),
-                    keyboardType: TextInputType.text,
-                    controller: controller.searchController,
-                    decoration: InputDecoration(
-                        hintText: MyStrings.searchByTrxId.tr,
-                        contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                        hintStyle: regularSmall.copyWith(color: MyColor.hintTextColor),
-                        filled: true,
-                        fillColor: MyColor.transparentColor,
-                        border: const OutlineInputBorder(borderSide: BorderSide(color: MyColor.colorGrey, width: 0.5)),
-                        enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: MyColor.colorGrey, width: 0.5)),
-                        focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: MyColor.primaryColor, width: 0.5))
+      builder: (controller) => Container(
+        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.only(bottom: Dimensions.space15),
+        padding: const EdgeInsets.symmetric(vertical: Dimensions.space15, horizontal: Dimensions.space15),
+        decoration: BoxDecoration(color: MyColor.getCardBgColor(), borderRadius: BorderRadius.circular(Dimensions.defaultRadius)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(MyStrings.transactionNo.tr, style: regularSmall.copyWith(color: MyColor.getLabelTextColor(), fontWeight: FontWeight.w500)),
+                  const SizedBox(height: Dimensions.space5),
+                  SizedBox(
+                    height: 45,
+                    width: MediaQuery.of(context).size.width,
+                    child: TextFormField(
+                      cursorColor: MyColor.primaryColor,
+                      style: regularSmall.copyWith(color: MyColor.colorBlack),
+                      keyboardType: TextInputType.text,
+                      controller: controller.searchController,
+                      decoration: InputDecoration(
+                          hintText: MyStrings.searchByTrxId.tr,
+                          contentPadding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                          hintStyle: regularSmall.copyWith(color: MyColor.hintTextColor),
+                          filled: true,
+                          fillColor: MyColor.transparentColor,
+                          border: const OutlineInputBorder(borderSide: BorderSide(color: MyColor.colorGrey, width: 0.5)),
+                          enabledBorder: const OutlineInputBorder(borderSide: BorderSide(color: MyColor.colorGrey, width: 0.5)),
+                          focusedBorder: const OutlineInputBorder(borderSide: BorderSide(color: MyColor.primaryColor, width: 0.5))
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          const SizedBox(width: Dimensions.space10),
-          InkWell(
-            onTap: () {
-              controller.filterData();
-            },
-            child: Container(
-              height: 45,
-              width: 45,
-              padding:
-              const EdgeInsets.all(4),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
-                color: MyColor.primaryColor,),
-              child: const Icon(
-                  Icons.search_outlined,
-                  color: MyColor.colorWhite,
-                  size: 18),
-            ),
-          )
-        ],
+            const SizedBox(width: Dimensions.space10),
+            InkWell(
+              onTap: () {
+                controller.filterData();
+              },
+              child: Container(
+                height: 45,
+                width: 45,
+                padding:
+                const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: MyColor.primaryColor,),
+                child: const Icon(
+                    Icons.search_outlined,
+                    color: MyColor.colorWhite,
+                    size: 18),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
